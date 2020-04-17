46a287ea8d7d59119f613e6ad70a9708967032ac
Kazuki Nishikawa <kazuki_nishikawa@voyagegroup.com>
2020-04-17 17:25:42 +0900

    [add] DATA STRAPから持ってきた

---
+++ src/components/Table/HeaderCell.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import Typography from "../Typography";
+import Flex from "../Flex";
+import { colors } from "../../styles/color";
+import { Size, Radius } from "../../styles";
+
+const Component = styled.th<{ width: string }>`
+  width: ${({ width }) => width};
+  padding: ${({ theme }) => theme.spacing * 2}px;
+  background-color: ${colors.basic[100]};
+  border-right: ${Size.Border.Normal} solid
+    ${({ theme }) => theme.palette.gray.light};
+  border-bottom: ${Size.Border.Small} solid
+    ${({ theme }) => theme.palette.gray.light};
+`;
+
+const RequiredBadge = styled.div`
+  padding: 2px 7px;
+  background: ${({ theme }) => theme.palette.danger.main};
+  border-radius: ${Radius.SMALL};
+`;
+
+export type Props = React.TdHTMLAttributes<HTMLTableDataCellElement> &
+  React.ThHTMLAttributes<HTMLTableHeaderCellElement> & {
+    width?: string;
+    children?: React.ReactNode;
+    required?: boolean;
+  };
+
+export const HeaderCell: React.FunctionComponent<Props> = ({
+  width = "auto",
+  children,
+  required = false,
+  ...rest
+}) => {
+  return (
+    <Component width={width} {...rest}>
+      <Flex display="flex" justifyContent="space-between">
+        <Typography weight="bold" size="md">
+          {children}
+        </Typography>
+        {required && (
+          <RequiredBadge>
+            <Typography color="white" weight="bold" size="xs">
+              必須
+            </Typography>
+          </RequiredBadge>
+        )}
+      </Flex>
+    </Component>
+  );
+};
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -4,11 +4,14 @@ import { Body } from "./Body";
 import { Cell } from "./Cell";
 import { Header } from "./Header";
 import { Row } from "./Row";
+import { HeaderCell } from "./HeaderCell";
+import { Size } from "../../styles";
 
 const Container = styled.table`
   width: 100%;
   border-collapse: collapse;
-  table-layout: fixed;
+  table-layout: auto;
+  border: ${Size.Border.Normal} solid ${({ theme }) => theme.palette.gray.light};
   background-color: ${({ theme }) => theme.palette.background.default};
 `;
 
@@ -22,5 +25,6 @@ Table.Header = Header;
 Table.Body = Body;
 Table.Row = Row;
 Table.Cell = Cell;
+Table.HeaderCell = HeaderCell;
 
 export default Table;
