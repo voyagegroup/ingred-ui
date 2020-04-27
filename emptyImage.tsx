bb67386019461a147052a29f483358c94805ba6a
Kazuki Nishikawa <kazuki_nishikawa@voyagegroup.com>
2020-04-27 12:51:46 +0900

    [add] EmptyImageにイラストを追加

---
+++ emptyImage.tsx
@@ -0,0 +1,145 @@
+import * as React from "react";
+
+function Svg10(props) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 138 137.926" {...props}>
+      <defs>
+        <clipPath id="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__a">
+          <path
+            className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__a"
+            d="M0 0h138v137.925H0z"
+          />
+        </clipPath>
+        <clipPath id="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__b">
+          <path
+            className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__a"
+            d="M68.539 0a68.813 68.813 0 1048.9 20.021A68.813 68.813 0 0068.539 0z"
+            transform="translate(-.1)"
+          />
+        </clipPath>
+        <style>
+          {
+            ".\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__a,.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__j,.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__k{fill:none}.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c{fill:#cdf0fe}.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__j,.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__k{stroke:#0b82f4;stroke-miterlimit:10;stroke-linecap:round;stroke-width:2.244px}.\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__l{fill:#0b82f4}"
+          }
+        </style>
+      </defs>
+      <g
+        transform="translate(0 .001)"
+        clipPath="url(#\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__a)"
+      >
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          d="M138 68.813a68.813 68.813 0 11-20.287-48.791A68.813 68.813 0 01138 68.813"
+        />
+      </g>
+      <g
+        transform="translate(.374)"
+        clipPath="url(#\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__b)"
+      >
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          d="M108.456 38.521v99.854a6.732 6.732 0 01-6.732 6.732h-66.2a6.732 6.732 0 01-6.727-6.732V30.668a6.732 6.732 0 016.732-6.358H93.87"
+        />
+        <path
+          d="M108.456 38.521v99.854a6.732 6.732 0 01-6.732 6.732h-66.2a6.732 6.732 0 01-6.727-6.732V30.668a6.732 6.732 0 016.732-6.358H93.87"
+          strokeWidth={1.87}
+          strokeMiterlimit={10}
+          strokeLinecap="square"
+          stroke="#0b82f4"
+          fill="none"
+        />
+        <path
+          d="M113.692 33.285v99.854a6.732 6.732 0 01-6.732 6.732h-66.2a6.732 6.732 0 01-6.727-6.732V25.432a6.732 6.732 0 016.732-6.732H99.48"
+          fill="#fff"
+        />
+        <path
+          d="M113.692 33.285v99.854a6.732 6.732 0 01-6.732 6.732h-66.2a6.732 6.732 0 01-6.727-6.732V25.432a6.732 6.732 0 016.732-6.732H99.48"
+          strokeWidth={1.87}
+          strokeMiterlimit={10}
+          strokeLinecap="square"
+          stroke="#0b82f4"
+          fill="none"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          d="M101.724 33.285h12.341L99.48 18.7v11.967a2.244 2.244 0 002.618 2.618"
+        />
+        <path
+          d="M101.724 33.285h12.341L99.48 18.7v11.967a2.244 2.244 0 002.244 2.618z"
+          strokeLinejoin="bevel"
+          strokeWidth={1.87}
+          stroke="#0b82f4"
+          fill="none"
+        />
+        <path
+          strokeWidth={2.244}
+          strokeLinecap="round"
+          strokeMiterlimit={10}
+          stroke="#0b82f4"
+          fill="none"
+          d="M103.967 42.635H44.504"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__j"
+          d="M65.821 63.204h37.772"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__k"
+          d="M65.821 68.814h13.837"
+        />
+        <rect
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          width={13.837}
+          height={13.688}
+          rx={2.618}
+          transform="translate(44.878 59.464)"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__l"
+          d="M56.097 60.587a1.5 1.5 0 011.5 1.5v8.6a1.5 1.5 0 01-1.5 1.5h-8.6a1.5 1.5 0 01-1.5-1.5v-8.978a1.5 1.5 0 011.5-1.5h8.6m0-2.244h-8.6a3.74 3.74 0 00-3.74 3.74v8.6a3.74 3.74 0 003.74 3.74h8.6a3.74 3.74 0 003.74-3.74v-8.6a3.74 3.74 0 00-3.74-3.74"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__j"
+          d="M65.821 90.505h37.772"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__k"
+          d="M65.821 96.115h13.837"
+        />
+        <rect
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          width={13.837}
+          height={13.688}
+          rx={2.618}
+          transform="translate(44.878 86.391)"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__l"
+          d="M56.097 87.513a1.5 1.5 0 011.5 1.5v8.6a1.5 1.5 0 01-1.5 1.5h-8.6a1.5 1.5 0 01-1.5-1.5v-8.6a1.5 1.5 0 011.5-1.5h8.6m0-2.244h-8.6a3.74 3.74 0 00-3.74 3.74v8.6a3.74 3.74 0 003.74 3.74h8.6a3.74 3.74 0 003.74-3.74v-8.6a3.74 3.74 0 00-3.74-3.74"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__j"
+          d="M65.821 117.806h37.772"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__k"
+          d="M65.821 123.416h13.837"
+        />
+        <rect
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__c"
+          width={13.837}
+          height={13.688}
+          rx={2.618}
+          transform="translate(44.878 113.692)"
+        />
+        <path
+          className="\u30A2\u30FC\u30C8\u30DB\u3099\u30FC\u30C8\u3099_10_svg__l"
+          d="M56.097 114.814a1.5 1.5 0 011.5 1.5v8.6a1.5 1.5 0 01-1.5 1.5h-8.6a1.5 1.5 0 01-1.5-1.5v-8.6a1.5 1.5 0 011.5-1.5h8.6m0-2.244h-8.6a3.74 3.74 0 00-3.74 3.74v8.6a3.74 3.74 0 003.74 3.74h8.6a3.74 3.74 0 003.74-3.74v-8.6a3.74 3.74 0 00-3.74-3.74"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default Svg10;
+
--- src/components/ItemEmpty/ItemEmpty.tsx
+++ src/components/ItemEmpty/ItemEmpty.tsx
@@ -2,15 +2,18 @@ import * as React from "react";
 import * as Styled from "./styled";
 import Spacer from "../Spacer";
 import Typography from "../Typography";
+import { EmptyImage } from "./internal/EmptyImage";
 
 type Props = {
   title: string;
   subtitle?: string;
 };
 
-// TODO: 画像入れる
 const ItemEmpty: React.FunctionComponent<Props> = ({ title, subtitle }) => (
   <Styled.EmptyContainer>
+    <Styled.EmptyImageContainer>
+      <EmptyImage />
+    </Styled.EmptyImageContainer>
     <Spacer pt={5} pb={1}>
       <Typography weight="bold" size="xxxl" align="center">
         {title}
