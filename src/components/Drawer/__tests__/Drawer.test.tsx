34410f0a404fa67f9b75a08fd8ade7790eb64389
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-05-26 08:03:11 +0900

    test追加

---
+++ src/components/Drawer/__tests__/Drawer.test.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { cleanup } from "@testing-library/react";
+import Drawer, {
+  DrawerContainer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerExpantionMenu,
+  DrawerExpantionMenuItem,
+  DrawerMenu,
+  DrawerFooter,
+  DrawerFixture,
+  DrawerMainContent,
+} from "..";
+import { renderWithThemeProvider } from "../../../utils/renderWithThemeProvider";
+
+describe("Drawer component testing", () => {
+  afterEach(cleanup);
+
+  test("Drawer", () => {
+    const { asFragment } = renderWithThemeProvider(
+      <DrawerContainer>
+        <Drawer>
+          <DrawerHeader>ここにロゴとかが入るよ</DrawerHeader>
+          <DrawerContent>
+            <DrawerExpantionMenu
+              title="設定"
+              isActive={true}
+              iconName="setting"
+              expantionList={[
+                <DrawerExpantionMenuItem
+                  isActive={true}
+                  title="デマンド設定"
+                />,
+              ]}
+            />
+            <DrawerMenu
+              title="ダッシュボード"
+              isActive={false}
+              iconName="dashboard"
+            />
+          </DrawerContent>
+          <DrawerFooter>
+            <DrawerFixture />
+          </DrawerFooter>
+        </Drawer>
+        <DrawerMainContent>hogehoge</DrawerMainContent>
+      </DrawerContainer>,
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
