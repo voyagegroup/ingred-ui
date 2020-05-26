37a66cd0e7cfd093f0a126d46060ad2d9b15be98
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-05-26 15:32:12 +0900

    example2をとりあえず作ってみた

---
+++ example2/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app_header">
+        <h1>React Countdown App Demo</h1>
+      </header>
+      <main className="cd_wrap">
+        <div>hoge</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
