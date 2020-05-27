7b5c873a82cdbf63f25318243171ac2592ace923
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-05-27 17:01:46 +0900

    tweak

--- src/components/DataTable/internal/Table/Header.tsx
+++ src/components/DataTable/internal/Table/Header.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import styled from "styled-components";
-import { colors } from "../../../../styles/color";
 import { Size } from "../../../../styles";
 
 export type Props = {};
@@ -9,7 +8,7 @@ const Component = styled.thead`
   border-top: ${Size.Border.Small} solid ${({ theme }) => theme.palette.divider};
   border-bottom: ${Size.Border.Small} solid
     ${({ theme }) => theme.palette.divider};
-  background-color: ${colors.basic[100]};
+  background-color: ${({ theme }) => theme.palette.gray.highlight};
 `;
 
 export const Header: React.FunctionComponent<Props> = ({ children }) => (
