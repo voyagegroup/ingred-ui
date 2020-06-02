80804f1092d83a616ff56bea8268be6cc5ebafd1
maktak1995 <takehiro_makita@voyagegroup.com>
2020-06-02 12:03:26 +0900

    ButtonGroupのKnobにdisabled切り替えを追加

--- src/components/ButtonGroup/ButtonGroup.stories.tsx
+++ src/components/ButtonGroup/ButtonGroup.stories.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import styled from "styled-components";
 import ButtonGroup from ".";
 import Button from "../Button";
+import { boolean } from "@storybook/addon-knobs";
 import { action } from "@storybook/addon-actions";
 import Typography from "../Typography";
 
@@ -23,27 +24,44 @@ export default {
   },
 };
 
-export const Overview = () => (
-  <Container>
-    <Typography weight="bold" size="xxl">
-      Medium Button
-    </Typography>
-    <RowContainer>
-      <ButtonGroup>
-        <Button onClick={action("clicked")}>保存する</Button>
-        <Button onClick={action("clicked")}>編集する</Button>
-      </ButtonGroup>
-    </RowContainer>
+export const Overview = () => {
+  const midiumButtonRight = boolean("Midium Button Right Disable", false);
+  const midiumButtonLeft = boolean("Midium Button Left Disable", false);
+  const smallButtonRight = boolean("Small Button Right Disable", false);
+  const smallButtonCenter = boolean("Small Button Center Disable", false);
+  const smallButtonLeft = boolean("Small Button Left Disable", false);
+  return (
+    <Container>
+      <Typography weight="bold" size="xxl">
+        Medium Button
+      </Typography>
+      <RowContainer>
+        <ButtonGroup>
+          <Button disabled={midiumButtonRight} onClick={action("clicked")}>
+            保存する
+          </Button>
+          <Button disabled={midiumButtonLeft} onClick={action("clicked")}>
+            編集する
+          </Button>
+        </ButtonGroup>
+      </RowContainer>
 
-    <Typography weight="bold" size="xxl">
-      Small Button
-    </Typography>
-    <RowContainer>
-      <ButtonGroup size="small">
-        <Button onClick={action("clicked")}>保存する</Button>
-        <Button onClick={action("clicked")}>編集する</Button>
-        <Button onClick={action("clicked")}>キャンセル</Button>
-      </ButtonGroup>
-    </RowContainer>
-  </Container>
-);
+      <Typography weight="bold" size="xxl">
+        Small Button
+      </Typography>
+      <RowContainer>
+        <ButtonGroup size="small">
+          <Button disabled={smallButtonRight} onClick={action("clicked")}>
+            保存する
+          </Button>
+          <Button disabled={smallButtonCenter} onClick={action("clicked")}>
+            編集する
+          </Button>
+          <Button disabled={smallButtonLeft} onClick={action("clicked")}>
+            キャンセル
+          </Button>
+        </ButtonGroup>
+      </RowContainer>
+    </Container>
+  );
+};
