cb412da267363011d88f4bba484a85e34d47180c
maktak1995 <takehiro_makita@voyagegroup.com>
2020-06-02 16:11:57 +0900

    全体disabledがfalseのときはchildrenはそのままつかう

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -42,7 +42,7 @@ const ButtonGroup: React.FunctionComponent<Props> = ({
         size === "small" ? `${fontSize["xs"]}px` : `${fontSize["md"]}px`
       }
     >
-      {childrenWithProps}
+      {disabled ? childrenWithProps : children}
     </Styled.ButtonGroupContainer>
   );
 };
