a04cb2a11de9c564f5cc2976e3d1e7d0273433a1
maktak1995 <takehiro_makita@voyagegroup.com>
2020-06-03 15:38:10 +0900

    SNapshotの更新

---
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import * as Styled from "./styled";
+import { ButtonSize } from "../Button/Button";
+
+type GroupButtonSize = Exclude<ButtonSize, "large">;
+
+type Props = {
+  size?: GroupButtonSize;
+  disabled?: boolean;
+};
+
+const buttonSize: Record<
+  GroupButtonSize,
+  { minWidth: string; height: string }
+> = {
+  small: {
+    minWidth: "63px",
+    height: "32px",
+  },
+  medium: {
+    minWidth: "71px",
+    height: "42px",
+  },
+};
+
+const ButtonGroup: React.FunctionComponent<Props> = ({
+  size = "medium",
+  disabled = false,
+  children,
+}) => {
+  const childProps = disabled
+    ? {
+        disabled: true,
+        size: size,
+        color: "secondary",
+      }
+    : {
+        // 各子要素のdisabledが使えなくなるので disabled:false は指定しない
+        size: size,
+        color: "secondary",
+      };
+
+  const childrenWithProps = React.Children.map(
+    children,
+    (child: React.ReactElement) => {
+      return React.cloneElement(child, childProps);
+    },
+  );
+  return (
+    <Styled.ButtonGroupContainer
+      height={buttonSize[size].height}
+      minWidth={buttonSize[size].minWidth}
+    >
+      {childrenWithProps}
+    </Styled.ButtonGroupContainer>
+  );
+};
+export default ButtonGroup;
