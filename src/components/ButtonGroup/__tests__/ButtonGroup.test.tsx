4a40da3206256bd9b3c4b88ea4ff26e58790bfeb
maktak1995 <takehiro_makita@voyagegroup.com>
2020-06-03 10:18:29 +0900

    testにchildrenを追加

--- src/components/ButtonGroup/__tests__/ButtonGroup.test.tsx
+++ src/components/ButtonGroup/__tests__/ButtonGroup.test.tsx
@@ -2,13 +2,19 @@ import * as React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { cleanup } from "@testing-library/react";
 import ButtonGroup from "..";
+import Button from "../../Button";
 import { renderWithThemeProvider } from "../../../utils/renderWithThemeProvider";
 
 describe("Button component testing", () => {
   afterEach(cleanup);
 
   test("ButtonGroup", () => {
-    const { asFragment } = renderWithThemeProvider(<ButtonGroup />);
+    const { asFragment } = renderWithThemeProvider(
+      <ButtonGroup>
+        <Button>編集する</Button>
+        <Button>保存する</Button>
+      </ButtonGroup>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
