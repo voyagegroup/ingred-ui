a053aad4c100edfba4371b7a260e5c982905b5ac
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-06-08 10:23:12 +0900

    tweak

--- src/components/ConfirmModal/__tests__/ConfirmModal.test.tsx
+++ src/components/ConfirmModal/__tests__/ConfirmModal.test.tsx
@@ -4,6 +4,14 @@ import { cleanup } from "@testing-library/react";
 import ConfirmModal from "..";
 import { renderWithThemeProvider } from "../../../utils/renderWithThemeProvider";
 
+jest.mock("react-dom", () => {
+  const original = jest.requireActual("react-dom");
+  return {
+    ...original,
+    createPortal: (node: any) => node,
+  };
+});
+
 describe("ConfirmModal component testing", () => {
   afterEach(cleanup);
 
