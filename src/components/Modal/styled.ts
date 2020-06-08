cb1ff1530baf9fd725dbee36003a9edcb7847fa9
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-06-08 20:06:03 +0900

    <Modal />をadd-popup-menuブランチから取ってくる

---
+++ src/components/Modal/styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  z-index: ${({ theme }) => theme.depth.modal};
+`;
