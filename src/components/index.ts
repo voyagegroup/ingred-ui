a211c27ea73da438582302098e088aaefc49fc2b
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-06-11 14:58:50 +0900

    Revert "<Toast />の追加 (#66)"
    This reverts commit 96dce2b0d4346db3e1bf183929a2a93b6a34f5d0.

--- src/components/index.ts
+++ src/components/index.ts
@@ -98,9 +98,6 @@ export * from "./Table";
 export { default as TextField } from "./TextField";
 export * from "./TextField";
 
-export { default as Toast } from "./Toast";
-export * from "./Toast";
-
 export { default as Tooltip } from "./Tooltip";
 export * from "./Tooltip";
 
