7eed5bd0c12879f74f1099e550c1dcf043208f71
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-06-15 16:47:43 +0900

    contextMenuのwidthを固定

---
+++ src/components/ContextMenu/styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import OriginalActionButton from "../ActionButton";
+
+export const ActionButton = styled(OriginalActionButton)`
+  width: 34px;
+  height: 34px;
+`;
