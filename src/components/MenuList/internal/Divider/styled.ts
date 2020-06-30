195904b13714c9e8ce8ee24283734bbb63af2c45
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-06-30 17:16:56 +0900

    dividerを独立させる

--- src/components/MenuList/internal/Divider/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Divider = styled.hr`
-  border: none;
-  margin: ${({ theme }) => `${theme.spacing}px ${theme.spacing * 2}px`};
-  height: 1px;
-  background-color: ${({ theme }) => theme.palette.divider};
-`;
