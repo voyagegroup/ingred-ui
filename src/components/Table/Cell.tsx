24bb9fe26cf873b3712f108687c8827b27bfab3e
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-07-15 18:37:50 +0900

    <Table />いい感じになった

--- src/components/Table/Cell.tsx
+++ src/components/Table/Cell.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import styled from "styled-components";
-import { Size } from "../../styles";
 
 const Component = styled.td<{ width: string }>`
   width: ${({ width }) => width};
   padding: ${({ theme }) => theme.spacing * 2}px;
-  border: ${Size.Border.Small} solid ${({ theme }) => theme.palette.divider};
 `;
 
 export type Props = React.TdHTMLAttributes<HTMLTableDataCellElement> &
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -6,17 +6,27 @@ import { Header } from "./Header";
 import { Row } from "./Row";
 import { HeaderCell } from "./HeaderCell";
 import { Radius, Size } from "../../styles";
+import { colors } from "../../styles/color";
 
 const Container = styled.table`
   width: 100%;
-  border-collapse: collapse;
   table-layout: auto;
   background-color: ${({ theme }) => theme.palette.background.default};
-  border: ${Size.Border.Small} solid ${({ theme }) => theme.palette.divider};
+  border: ${Size.Border.Small} solid ${colors.basic[300]};
   border-radius: ${Radius.SMALL};
   border-collapse: separate;
   border-spacing: 0;
   overflow: hidden;
+
+  & tr:not(:last-child) > th,
+  & tr:not(:last-child) > td {
+    border-bottom: ${Size.Border.Small} solid ${colors.basic[300]};
+  }
+
+  & tr > th:not(:last-child),
+  & tr > td:not(:last-child) {
+    border-right: ${Size.Border.Small} solid ${colors.basic[300]};
+  }
 `;
 
 export type Props = {
