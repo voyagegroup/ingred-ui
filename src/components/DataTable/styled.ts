9f8fe6368deca8d25d19ac6e868e7c3c901ecb71
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-07-16 22:11:01 +0900

    tweak

--- src/components/DataTable/styled.ts
+++ src/components/DataTable/styled.ts
@@ -7,8 +7,6 @@ export const Container = styled.div`
 `;
 
 export const TableContainer = styled.div<{ fullWidth?: boolean }>`
-  /* TODO: 本当は1pxだが、内部の要素がはみ出るので2pxにしている */
-  /*       overflow: hiddenを足すとskickyにできない */
   border: ${({ fullWidth }) =>
     fullWidth ? "none" : `${Size.Border.Small} solid ${colors.basic[300]}`};
   border-top: ${Size.Border.Small} solid ${colors.basic[300]};
