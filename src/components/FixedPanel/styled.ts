fd4bb93e9ea8ec56d1bba91bb8fe4b6119174cfe
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-07-18 14:56:34 +0900

    hexToRgba通す

--- src/components/FixedPanel/styled.ts
+++ src/components/FixedPanel/styled.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { Size } from "../../styles";
+import { hexToRgba } from "../../utils/hexToRgba";
 
 type ContainerProps = {
   isOpen: boolean;
@@ -14,7 +15,8 @@ export const Container = styled.div<ContainerProps>`
     `${placement}: ${isOpen ? offset : -height - 10}px`};
   width: 100%;
   backdrop-filter: blur(2px);
-  background-color: ${({ theme }) => theme.palette.background.default}E6;
+  background-color: ${({ theme }) =>
+    hexToRgba(theme.palette.background.default, 0.9)};
   border-top: ${Size.Border.Small} solid ${({ theme }) => theme.palette.divider};
   border-bottom: ${Size.Border.Small} solid
     ${({ theme }) => theme.palette.divider};
