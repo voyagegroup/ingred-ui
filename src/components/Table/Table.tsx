2ad892a29dc10e41c6bf8b2e49b945d165894d78
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-07-27 14:01:14 +0900

    <Table />を角付きに戻す

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,29 +5,12 @@ import { Cell } from "./Cell";
 import { Header } from "./Header";
 import { Row } from "./Row";
 import { HeaderCell } from "./HeaderCell";
-import { Radius, Size } from "../../styles";
 
 const Container = styled.table`
   width: 100%;
+  border-collapse: collapse;
   table-layout: auto;
   background-color: ${({ theme }) => theme.palette.background.default};
-  border: ${Size.Border.Small} solid ${({ theme }) => theme.palette.divider};
-  border-radius: ${Radius.SMALL};
-  border-collapse: separate;
-  border-spacing: 0;
-  overflow: hidden;
-
-  & tr:not(:first-child) > th,
-  & tr:not(:first-child) > td {
-    border-top: ${Size.Border.Small} solid
-      ${({ theme }) => theme.palette.divider};
-  }
-
-  & tr > th:not(:last-child),
-  & tr > td:not(:last-child) {
-    border-right: ${Size.Border.Small} solid
-      ${({ theme }) => theme.palette.divider};
-  }
 `;
 
 export type Props = {
