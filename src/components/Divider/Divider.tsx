ce02871e758b9f1cc1f515ab4770bf695343ee71
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-07-29 15:34:07 +0900

    Divider縦線対応

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -5,15 +5,26 @@ import { SpacerProps } from "../../utils/spacerUtils";
 
 type Props = SpacerProps & {
   color?: string;
+  orientation?: "vertical" | "horizontal";
+  isFlexItem?: boolean;
 };
 
 const Divider: React.FunctionComponent<Props> = ({
   color: colorProp,
+  orientation = "horizontal",
+  isFlexItem = false,
   ...rest
 }) => {
   const theme = useTheme();
   const color = colorProp || theme.palette.divider;
-  return <Styled.Divider color={color} {...rest} />;
+  return (
+    <Styled.Divider
+      color={color}
+      orientation={orientation}
+      isFlexItem={isFlexItem}
+      {...rest}
+    />
+  );
 };
 
 export default Divider;
--- src/components/Divider/styled.ts
+++ src/components/Divider/styled.ts
@@ -3,11 +3,23 @@ import { SpacerProps, spacer } from "../../utils/spacerUtils";
 
 type DividerProps = SpacerProps & {
   color: string;
+  orientation: "vertical" | "horizontal";
+  isFlexItem: boolean;
 };
 
 export const Divider = styled.hr<DividerProps>`
   ${spacer}
-  height: 1px;
+  width: ${({ orientation }) => (orientation === "vertical" ? "1px" : "auto")};
+  height: ${({ orientation, isFlexItem }) => {
+    switch (true) {
+      case isFlexItem:
+        return "auto";
+      case orientation === "vertical":
+        return "100%";
+      default:
+        return "1px";
+    }
+  }};
   border: none;
   background-color: ${({ color }) => color};
 `;
