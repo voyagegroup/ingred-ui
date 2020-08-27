dc4a6cccb57f1be342c4b89cf9d137569d7f3446
maktak1995 <takehiro_makita@voyagegroup.com>
2020-08-27 18:11:05 +0900

    PaletteText.secondaryをBasic[700]に変更

--- src/themes/palette.ts
+++ src/themes/palette.ts
@@ -84,7 +84,7 @@ export const palette: Palette = {
   },
   text: {
     primary: colors.blue[500],
-    secondary: colors.basic[600],
+    secondary: colors.basic[700],
     disabled: colors.basic[300],
     hint: colors.basic[400],
     white: "#FFFFFF",
