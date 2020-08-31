e45b5b6d83adba1f189db993adfc86e88bec3a55
suzuken <suzuken326@gmail.com>
2020-08-31 15:33:24 +0900

    storybook: add examples for Typography

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 import Typography from "./";
 
 export default {
@@ -8,4 +9,60 @@ export default {
   },
 };
 
-export const Overview = () => <Typography>テキストコンポーネント</Typography>;
+const Container = styled.div`
+  padding: ${({ theme }) => theme.spacing * 3}px;
+  background-color: ${({ theme }) => theme.palette.background.dark};
+`;
+
+const RowContainer = styled.div`
+  margin: ${({ theme }) => theme.spacing * 3}px;
+  padding: ${({ theme }) => theme.spacing * 3}px;
+  background-color: ${({ theme }) => theme.palette.background.default};
+  button + button {
+    margin-left: ${({ theme }) => theme.spacing * 5}px;
+  }
+`;
+
+const colors = ["initial", "primary", "secondary", "disabled", "hint", "white"];
+const sizes = [
+  "xs",
+  "sm",
+  "md",
+  "lg",
+  "xl",
+  "xxl",
+  "xxxl",
+  "xxxxl",
+  "xxxxxl",
+  "xxxxxxl",
+  "xxxxxxxl",
+];
+const aligns = ["left", "center", "right"];
+
+export const Overview = () => (
+  <Container>
+    <h2>Sizes</h2>
+    <RowContainer>
+      {sizes.map((s) => (
+        <Typography size={s}>{s}</Typography>
+      ))}
+    </RowContainer>
+    <h2>Colors</h2>
+    <RowContainer>
+      {colors.map((c) => (
+        <Typography color={c}>{c}</Typography>
+      ))}
+    </RowContainer>
+    <h2>Weight</h2>
+    <RowContainer>
+      <Typography weight={"normal"}>normal</Typography>
+      <Typography weight={"bold"}>bold</Typography>
+    </RowContainer>
+    <h2>Align</h2>
+    <RowContainer>
+      {aligns.map((a) => (
+        <Typography align={a}>{a}</Typography>
+      ))}
+    </RowContainer>
+  </Container>
+);
