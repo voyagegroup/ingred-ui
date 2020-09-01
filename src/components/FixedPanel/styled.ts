75e56b0bff830d0e35b183c54cc15ebac71ce700
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-09-01 16:19:38 +0900

    fixedPanel微修正

--- src/components/FixedPanel/styled.ts
+++ src/components/FixedPanel/styled.ts
@@ -20,7 +20,7 @@ export const Container = styled.div<ContainerProps>`
   width: 100%;
   background-color: ${({ theme }) =>
     hexToRgba(theme.palette.background.default, 0.9)};
-  border-top: 1px solid ${({ theme }) => theme.palette.divider};
+  border-top: 1px solid ${({ theme }) => theme.palette.gray.light};
   border-bottom: 1px solid ${({ theme }) => theme.palette.gray.light};
   transition: all 0.3s;
 `;
