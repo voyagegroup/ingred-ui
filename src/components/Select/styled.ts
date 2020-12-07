1cf35397fbda9bdef760e244cbf5a75d9e9e0208
林祐太郎 <40020812+yutaro1031@users.noreply.github.com>
2020-12-07 16:18:45 +0900

    tweak (#216)

--- src/components/Select/styled.ts
+++ src/components/Select/styled.ts
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import * as React from "react";
+import styled, { DefaultTheme, StyledComponent } from "styled-components";
 import { components, MenuListComponentProps } from "react-select";
 import { addScrollbarProperties } from "../../utils/scrollbar";
 
@@ -6,8 +7,14 @@ export const Container = styled.div<{ minWidth?: string }>`
   min-width: ${({ minWidth }) => minWidth || "auto"};
 `;
 
-export const ReactSelectMenuList = styled(components.MenuList)<
-  MenuListComponentProps<any>
->`
+// MEMO: Add type annotations cause of Error↓
+//       The inferred type of 'ReactSelectMenuList' cannot be named without a reference to 'styled-components/node_modules/@types/react'.
+//       This is likely not portable. A type annotation is necessary.
+export const ReactSelectMenuList: StyledComponent<
+  React.FunctionComponent<MenuListComponentProps<any>>,
+  DefaultTheme,
+  {},
+  never
+> = styled(components.MenuList)<MenuListComponentProps<any>>`
   ${({ maxHeight }) => addScrollbarProperties({ maxHeight: `${maxHeight}px` })}
 `;
