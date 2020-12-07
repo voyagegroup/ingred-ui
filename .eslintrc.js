022fff344dc30c776b5adbdfc9cad37ab5e1f336
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-12-07 12:17:12 +0900

    tweak

--- .eslintrc.js
+++ .eslintrc.js
@@ -168,17 +168,6 @@ module.exports = {
       },
     },
     {
-      files: ["*.md"],
-      rules: {
-        "prettier/prettier": [
-          2,
-          {
-            parser: "markdown",
-          },
-        ],
-      },
-    },
-    {
       files: ["*.mdx"],
       extends: ["plugin:mdx/overrides"],
     },
