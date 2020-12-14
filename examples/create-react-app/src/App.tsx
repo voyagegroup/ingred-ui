013f9e3a3c6e91ccf9637bebabca52b3986a715b
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2020-12-14 17:39:01 +0900

    総じていい感じになった

--- examples/create-react-app/src/App.tsx
+++ examples/create-react-app/src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  ThemeProvider,
+  createTheme,
+  Typography,
+  Spacer
+} from 'ingred-ui';
 
 function App() {
+  const theme = createTheme();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Typography component="h1" size="xxxxxl" align="center">
+        Create React App example
+      </Typography>
+      <Spacer pt={2} />
+      <Typography color="secondary" size="xxl" align="center">
+        Write some components in this environment.
+      </Typography>
+      <Spacer pt={2} />
+      <Typography color="secondary" size="xxl" align="center">
+        Copyright © <a href="https://ingred-ui.netlify.app/">Your Website</a> 2020.
+      </Typography>
+    </ThemeProvider>
   );
 }
 
--- examples/create-react-app/src/index.tsx
+++ examples/create-react-app/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
