884bba10c919d908cd8d25004f8bb1442e25878f
ryokosuge <ryo.kosuge@gmail.com>
2021-01-06 17:11:48 +0900

    feat:	logoを表示

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,7 +1,8 @@
 import { create } from "@storybook/theming/create";
+import logo from "../assets/logo.svg";
 
 export default create({
   base: "light",
-  brandImage: "logo.svg",
+  brandImage: logo,
   brandTitle: "INGRED UI",
 });
