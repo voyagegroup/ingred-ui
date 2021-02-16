305546c4d24c7cb4576744a01840cfd98c1ae88b
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2021-02-16 17:18:44 +0900

    RadioButtonいい感じになった

--- src/components/RadioButton/RadioButton.stories.tsx
+++ src/components/RadioButton/RadioButton.stories.tsx
@@ -1,53 +1,37 @@
-import { action } from "@storybook/addon-actions";
-
 import React from "react";
-import RadioButton, { RadioButtonSize } from "./";
-import Spacer from "../Spacer";
+import { Story } from "@storybook/react/types-6-0";
+import RadioButton, { RadioButtonProps, RadioButtonSize } from "./";
 
 export default {
   title: "Components/Inputs/RadioButton",
   component: RadioButton,
-  parameters: {
-    docs: { page: null },
+  args: {
+    size: RadioButtonSize.MEDIUM,
   },
 };
 
-export const Overview = () => (
+export const Example: Story<RadioButtonProps> = (args) => (
   <>
-    <RadioButton
-      defaultChecked={true}
-      name="group"
-      onChange={action("onChange1")}
-    >
-      ラジオボタン
-    </RadioButton>
-    <Spacer mt={1} />
-    <RadioButton name="group" onChange={action("onChange2")}>
-      ラジオボタン
-    </RadioButton>
-    <Spacer mt={1} />
-    <RadioButton disabled={true} onChange={action("onChange2")}>
-      ラジオボタン
-    </RadioButton>
-    <Spacer mt={1} />
-    <RadioButton disabled={true} checked={true} onChange={action("onChange2")}>
-      ラジオボタン
-    </RadioButton>
+    <RadioButton {...args}>One</RadioButton>
+    <br />
+    <RadioButton {...args}>Two</RadioButton>
   </>
 );
 
-export const WithControled = () => (
-  <RadioButton checked name="group" onChange={action("onChange")} />
-);
+Example.args = {
+  name: "example",
+};
 
-export const WithLongLabel = () => (
+export const DesignSample = () => (
   <>
-    <RadioButton
-      name="group"
-      size={RadioButtonSize.MEDIUM}
-      onChange={action("onChange")}
-    >
-      {"長いラベル ".repeat(15).trim()}
+    <RadioButton>Basic</RadioButton>
+    <br />
+    <RadioButton checked={true}>Selected</RadioButton>
+    <br />
+    <RadioButton disabled={true}>Disabled</RadioButton>
+    <br />
+    <RadioButton disabled={true} checked={true}>
+      Disabled(checked)
     </RadioButton>
   </>
 );
