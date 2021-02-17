2886aedfed511c41d44a8315f291891c2ef00d2e
林祐太郎 <40020812+yutaro1031@users.noreply.github.com>
2021-02-17 09:47:30 +0900

    document component vol9 (#246)
    * Storybookいい感じに
    
    * tweak
    
    * Typographyいい感じに
    
    * ButtonGroupいい感じにする
    
    * Checkboxいい感じにした
    
    * tweak
    
    * DatePicker系をいい感じにした

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -1,77 +1,28 @@
 import * as React from "react";
-import styled from "styled-components";
+import { Story } from "@storybook/react/types-6-0";
 import Typography from "./";
-import { TextAlign, FontSize } from "./Typography";
+import { TypographyProps } from "./Typography";
 
 export default {
   title: "Components/Data Display/Typography",
   component: Typography,
-  parameters: {
-    docs: { page: null },
+  argTypes: {
+    color: {
+      control: {
+        type: "select",
+        options: [
+          "initial",
+          "primary",
+          "secondary",
+          "disabled",
+          "hint",
+          "white",
+        ],
+      },
+    },
   },
 };
 
-const Container = styled.div`
-  padding: ${({ theme }) => theme.spacing * 3}px;
-  background-color: ${({ theme }) => theme.palette.background.dark};
-`;
-
-const RowContainer = styled.div`
-  margin: ${({ theme }) => theme.spacing * 3}px;
-  padding: ${({ theme }) => theme.spacing * 3}px;
-  background-color: ${({ theme }) => theme.palette.background.default};
-  button + button {
-    margin-left: ${({ theme }) => theme.spacing * 5}px;
-  }
-`;
-
-const colors = ["initial", "primary", "secondary", "disabled", "hint", "white"];
-const sizes: FontSize[] = [
-  "xs",
-  "sm",
-  "md",
-  "lg",
-  "xl",
-  "xxl",
-  "xxxl",
-  "xxxxl",
-  "xxxxxl",
-  "xxxxxxl",
-  "xxxxxxxl",
-];
-
-const aligns: TextAlign[] = ["left", "center", "right"];
-
-export const Overview = () => (
-  <Container>
-    <h2>Sizes</h2>
-    <RowContainer>
-      {sizes.map((s) => (
-        <Typography key={s} size={s}>
-          {s}
-        </Typography>
-      ))}
-    </RowContainer>
-    <h2>Colors</h2>
-    <RowContainer>
-      {colors.map((c) => (
-        <Typography key={c} color={c}>
-          {c}
-        </Typography>
-      ))}
-    </RowContainer>
-    <h2>Weight</h2>
-    <RowContainer>
-      <Typography weight={"normal"}>normal</Typography>
-      <Typography weight={"bold"}>bold</Typography>
-    </RowContainer>
-    <h2>Align</h2>
-    <RowContainer>
-      {aligns.map((a) => (
-        <Typography key={a} align={a}>
-          {a}
-        </Typography>
-      ))}
-    </RowContainer>
-  </Container>
+export const Example: Story<TypographyProps> = (args) => (
+  <Typography {...args}>Typography</Typography>
 );
