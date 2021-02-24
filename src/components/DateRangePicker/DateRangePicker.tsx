59a958e525ff1dcea09b9f3df8ba703be8120bf2
yutaro1031 <40020812+yutaro1031@users.noreply.github.com>
2021-02-24 10:05:43 +0900

    日付選択系のコンポーネントをとりあえず英語化

--- src/components/DateRangePicker/DateRangePicker.tsx
+++ src/components/DateRangePicker/DateRangePicker.tsx
@@ -9,18 +9,6 @@ import {
 } from "react-dates";
 import Icon from "../Icon";
 
-moment.locale("ja", {
-  weekdaysShort: ["日", "月", "火", "水", "木", "金", "土"],
-});
-
-function renderMonthText(day: moment.Moment) {
-  return day.format("YYYY年M月");
-}
-
-function displayFormat() {
-  return "YYYY/MM/DD";
-}
-
 function isOutsideRange() {
   return false;
 }
@@ -52,12 +40,10 @@ const DateRangePicker: React.FunctionComponent<DateRangePickerProps> = ({
         startDatePlaceholderText="FROM"
         endDatePlaceholderText="TO"
         isOutsideRange={isOutsideRange}
-        displayFormat={displayFormat}
         customArrowIcon={<Styled.CustomArrowIcon />}
         numberOfMonths={2}
         enableOutsideDays={true}
         daySize={41}
-        renderMonthText={renderMonthText}
         weekDayFormat="ddd"
         hideKeyboardShortcutsPanel={true}
         startDateId="startDate"
