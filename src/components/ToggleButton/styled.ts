79eaf11407ae5253d61776c619d55ea1fd5b04fa
Hiroki Kondo <50824354+hirokikondo86@users.noreply.github.com>
2021-03-25 15:20:23 +0900

    Revert "トグルボタンのデザインを変更"

--- src/components/ToggleButton/styled.ts
+++ src/components/ToggleButton/styled.ts
@@ -1,7 +1,8 @@
 import styled, { css } from "styled-components";
-import { hexToRgba } from "../../utils/hexToRgba";
 
 export const Container = styled.div<{
+  active: boolean;
+  disabled: boolean;
   width: string;
 }>`
   position: relative;
@@ -10,14 +11,14 @@ export const Container = styled.div<{
 `;
 
 export const ToggleButton = styled.span<{ active: boolean; disabled: boolean }>`
+  content: "";
   position: absolute;
-  top: 50%;
-  left: 4px;
-  z-index: 9999;
-  transform: translateY(-50%);
-  width: 14px;
-  height: 14px;
-  border-radius: 14px;
+  top: 2px;
+  left: 2px;
+  width: 18px;
+  height: 18px;
+  border-radius: 18px;
+  transition: 0.2s;
   background-color: ${({ active, disabled, theme }) => {
     let backgroundColor = theme.palette.background.default;
     if (disabled) {
@@ -29,31 +30,16 @@ export const ToggleButton = styled.span<{ active: boolean; disabled: boolean }>`
   }};
   border: 1px solid
     ${({ active, disabled, theme }) =>
-      active && !disabled ? theme.palette.primary.dark : theme.palette.divider};
-  box-shadow: ${({ theme }) =>
-    `0 -2px ${hexToRgba(theme.palette.black, 0.16)} inset, 0px 1px ${hexToRgba(
-      theme.palette.black,
-      0.08,
-    )}`};
-  transition: all 0.3s cubic-bezier(0.47, 0, 0.75, 0.72);
-`;
+      active && !disabled ? theme.palette.primary.main : theme.palette.divider};
 
-export const LabelText = styled.div`
-  position: absolute;
-  width: 100%;
-  word-break: break-all;
-  white-space: nowrap;
-  transition: all 0s 0.3s cubic-bezier(0.47, 0, 0.75, 0.72);
-`;
-
-export const ActiveLabelText = styled(LabelText)`
-  padding-right: 6px;
-  opacity: 0;
-`;
-
-export const InActiveLabelText = styled(LabelText)`
-  margin-left: 6px;
-  opacity: 1;
+  ${({ active }) =>
+    active &&
+    css`
+      & {
+        left: calc(100% - 2px);
+        transform: translateX(-100%);
+      }
+    `}
 `;
 
 type LabelProps = {
@@ -72,40 +58,33 @@ export const Label = styled.label<LabelProps>`
   cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
   width: ${({ width }) => width};
   height: calc(1px * 2 + 22px);
-  background-color: ${({ active, disabled, theme }) => {
-    let backgroundColor = theme.palette.gray.highlight;
-    if (disabled) {
-      backgroundColor = theme.palette.gray.light;
-    } else if (active) {
-      backgroundColor = theme.palette.background.hint;
-    }
-    return backgroundColor;
-  }};
+  background-color: ${({ active, disabled, theme }) =>
+    active && !disabled
+      ? theme.palette.background.hint
+      : theme.palette.gray.light};
   border: 1px solid
     ${({ active, disabled, theme }) =>
       active && !disabled ? theme.palette.primary.main : theme.palette.divider};
   border-radius: 56px;
-  box-shadow: ${({ theme }) =>
-    `0 2px ${hexToRgba(theme.palette.black, 0.08)} inset`};
-  transition: all 0.3s ease, border-color 0.3s cubic-bezier(0.47, 0, 0.75, 0.72);
+  transition: background-color 0.2s, border-color 0.2s;
 
-  ${({ active }) =>
-    active &&
-    css`
-      & > ${ToggleButton} {
-        left: calc(100% - 14px - 4px);
-      }
-      & > ${ActiveLabelText} {
-        opacity: 1;
-      }
-      & > ${InActiveLabelText} {
-        opacity: 0;
-      }
-    `}
+  &:active > ${ToggleButton} {
+    width: 22px;
+  }
 `;
 
 export const HiddenInput = styled.input`
   width: 0;
   height: 0;
   visibility: hidden;
+`;
+
+export const LabelText = styled.div<{ position: "right" | "left" }>`
+  position: absolute;
+  top: 50%;
+  ${({ position }) => `${position}: calc(50% - 7px)`};
+  transform: translate(
+    ${({ position }) => (position === "right" ? "" : "-")}50%,
+    -50%
+  );
 `;
