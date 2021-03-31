8db52e337845d3f83e6c3a2a6c8aefbd8c46ddb8
penicillin0 <ayahitonakamura@gmail.com>
2021-03-31 15:45:21 +0900

    ラベルと型の作成

---
+++ src/components/MultipleFilter/internal/Label/styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding-right: 8px;
+  padding-left: 8px;
+  width: auto;
+  height: 24px;
+  background-color: ${({ theme }) => theme.palette.gray.highlight};
+  border: 1px solid ${({ theme }) => theme.palette.divider};
+  border-radius: ${({ theme }) => theme.radius}px;
+  color: ${({ theme }) => theme.palette.black};
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RightContainer = styled.div`
+  padding-left: 10px;
+`;
