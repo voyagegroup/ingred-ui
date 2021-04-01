554a49da21b265dd3b12b4bc8845f47c952789a4
penicillin0 <ayahitonakamura@gmail.com>
2021-04-01 18:45:16 +0900

    for test

--- src/components/Icon/internal/CloseCircleIcon/index.tsx
+++ src/components/Icon/internal/CloseCircleIcon/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { IconProps } from "../../Icon";
 
-const CloseCircleIcon: React.FunctionComponent<IconProps> = ({ fill }) => {
+const CloseCircleIcon: React.FunctionComponent<IconProps> = () => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
       <g transform="translate(-657 -463)">
