921c70bd9446ae14f5956c82c9d33abebd77491a
penicillin0 <ayahitonakamura@gmail.com>
2021-04-09 12:59:37 +0900

    ラベルの編集ができるように

---
+++ src/components/MultipleFilter/internal/EditFilterCard/EditFilterCard.tsx
@@ -0,0 +1,162 @@
+import * as React from "react";
+import { useTheme } from "styled-components";
+import Button from "../../../Button";
+import { Divider } from "../../../Divider/styled";
+import Icon from "../../../Icon";
+import Select, { OptionType } from "../../../Select";
+import Spacer from "../../../Spacer";
+import Typography from "../../../Typography";
+import {
+  FilterPackType,
+  FilterType,
+  ReferedFilterType,
+  Types,
+} from "../../types";
+import * as Styled from "./styled";
+import { useForm } from "react-hook-form";
+import TextField from "../../../TextField";
+import ErrorText from "../../../ErrorText";
+
+export type Props = {
+  onClose: () => void;
+  onEdit: (editedReferedFilter: ReferedFilterType) => void;
+  willEditFilter?: ReferedFilterType;
+  selectedFilterPack?: FilterPackType;
+};
+
+type FormType = {
+  section: string;
+  condition: string;
+};
+
+export const EditFilterCard: React.FunctionComponent<Props> = ({
+  onClose,
+  onEdit,
+  willEditFilter,
+  selectedFilterPack,
+}) => {
+  const theme = useTheme();
+  const { register, setValue, handleSubmit, errors, control } = useForm({
+    shouldUnregister: false,
+    defaultValues: {
+      section: willEditFilter?.filterName,
+      condition: willEditFilter?.filterCondtion,
+    },
+  });
+  const [submitError, setSubmitError] = React.useState<string | undefined>(
+    undefined,
+  );
+
+  const handleSelect = (option: OptionType<any>) => {
+    if (option === null) {
+      setValue("condition", undefined);
+    } else {
+      setValue("condition", option.value);
+    }
+  };
+
+  const getInputField = (filter: FilterType) => {
+    const type = filter?.control.type;
+    switch (type) {
+      case "text":
+        return (
+          <TextField
+            inputRef={register({ required: true })}
+            name="condition"
+            errorText={errors.condition ? "入力してください" : ""}
+          />
+        );
+      case "select":
+        const options = filter?.control.options as string[];
+
+        return (
+          <Select
+            options={options.map((option) => ({
+              label: option,
+              value: option,
+            }))}
+            defaultValue={{
+              label: willEditFilter?.filterCondtion,
+              value: willEditFilter?.filterCondtion,
+            }}
+            onChange={handleSelect}
+          />
+        );
+      case "boolean":
+        return (
+          <TextField
+            inputRef={register({ required: true })}
+            name="condition"
+            errorText={errors.condition ? "入力してください" : ""}
+          />
+        );
+    }
+  };
+
+  const onSubmit = (data: FormType) => {
+    if (data.condition) {
+      setSubmitError(undefined);
+    } else {
+      setSubmitError("区分・状態を設定してください");
+      return;
+    }
+
+    const editedFilter: ReferedFilterType = {
+      filterCondtion: data.condition,
+      filterType: willEditFilter?.filterType as Types,
+      filterName: willEditFilter?.filterName as string,
+      categoryName: willEditFilter?.categoryName as string,
+    };
+    onEdit(editedFilter);
+  };
+
+  return (
+    <Styled.FilterCard>
+      <Styled.FilterCardHeader>
+        <Typography weight="bold" size="xxl">
+          {willEditFilter?.categoryName}
+        </Typography>
+
+        <Spacer pr={2} />
+        <Styled.CloseIconContainer key={"close"} onClick={onClose}>
+          <Icon name="close" size="md" color={theme.palette.black} />
+        </Styled.CloseIconContainer>
+      </Styled.FilterCardHeader>
+      <Styled.FilterContent>
+        <Typography weight="bold" size="lg">
+          区分
+        </Typography>
+        <Spacer py={0.5} />
+        <TextField readOnly value={willEditFilter?.filterName} />
+        <Spacer py={1} />
+        <Typography weight="bold" size="lg">
+          状態
+        </Typography>
+        <Spacer py={0.5} />
+        {getInputField(
+          selectedFilterPack?.filters.find(
+            (filter) => filter.filterName === willEditFilter?.filterName,
+          ) as FilterType,
+        )}
+        <Spacer py={1} />
+        <Divider
+          orientation="horizontal"
+          color={theme.palette.divider}
+          my={2}
+        />
+
+        {submitError && (
+          <Spacer py={2}>
+            <ErrorText>{submitError}</ErrorText>
+          </Spacer>
+        )}
+
+        <Styled.ButtonContainer>
+          <Button size="small" inline={true} onClick={handleSubmit(onSubmit)}>
+            変更する
+          </Button>
+        </Styled.ButtonContainer>
+      </Styled.FilterContent>
+    </Styled.FilterCard>
+  );
+};
---
+++ src/components/MultipleFilter/internal/EditFilterCard/index.ts
@@ -0,0 +1,1 @@
+export { EditFilterCard } from "./EditFilterCard";
