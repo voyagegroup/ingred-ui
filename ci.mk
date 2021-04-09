18b6bcd9284c9fc3a77b972804b119781c2fb528
ryokosuge <ryo.kosuge@gmail.com>
2021-04-09 10:51:44 +0900

    feat:	release

--- ci.mk
+++ ci.mk
@@ -1,6 +1,8 @@
-.PHONY: install test publish
+.PHONY: install test changelog publish
 
 SEMVER :=
+RELEASE_VERSION :=
+GITHUB_TOKEN :=
 
 __req_semver:
 ifeq ($(filter $(SEMVER),patch minor major),)
@@ -23,8 +25,17 @@ test:
 lint:
 	yarn lint
 
+changelog:
+	github_changelog_generator \
+		--user voyagegroup \
+		--project ingred-ui \
+		--exclude-labels release \
+		--future-release v${RELEASE_VERSION} \
+		--token ${FLUCT_MEMBER_GITHUB_TOKEN}
+
+release_version:
+	npm config set git-tag-version false
+	npm version ${RELEASE_VERSION}
+
 publish: __req_semver build
-	npm version ${SEMVER} -m "[ci skip] %s"
-	git push origin master
-	git push origin --tags
 	npm publish
