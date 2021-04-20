b8634d419b271541e853086a0d3d1fecff64550c
penicillin0 <56141035+penicillin0@users.noreply.github.com>
2021-04-20 14:14:32 +0900

    Select label design (#292)
    * 左右は8pxに
    
    * test update
    
    * closebuttonを追加
    
    * ラベルの追加
    
    * 5px->8px
    
    * close cursor変更
    
    * wip
    
    * サイズ微調整
    
    * デザイン修正
    
    * tweak
    
    * Revert "close cursor変更"
    
    This reverts commit 308c6429b98fa22c15c64431694987238f38917d.
    
    * Revert "5px->8px"
    
    This reverts commit 1b0a045df17c616b9b94e0494bd50905ab9f58ff.
    
    * Revert "ラベルの追加"
    
    This reverts commit f7f7c8246f1edc3724eb04deb653aeb7fbc38ce0.
    
    * test update
    
    * Revert "closebuttonを追加"
    
    This reverts commit c9b983bda83ca6f60a5bdc6c39bf0d7601294650.

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -110,7 +110,10 @@ const getOverrideStyles = (theme: Theme, error: boolean) => {
     },
     multiValue: (base) => ({
       ...base,
-      backgroundColor: theme.palette.gray.light,
+      backgroundColor: theme.palette.gray.highlight,
+      padding: "2px 0px 3px 2px",
+      margin: "0px 4px",
+      border: `1px solid ${theme.palette.divider}`,
       borderRadius: `${theme.radius}px`,
     }),
     multiValueLabel: (base, { isDisabled }) => {
@@ -130,12 +133,13 @@ const getOverrideStyles = (theme: Theme, error: boolean) => {
     multiValueRemove: (base) => ({
       ...base,
       ":hover": {
-        backgroundColor: theme.palette.gray.main,
+        backgroundColor: "transparent",
       },
+      padding: "0px 8px",
     }),
     valueContainer: (base) => ({
       ...base,
-      padding: "10px 8px",
+      minHeight: "40px",
     }),
   };
   return overrideStyles;
--- src/components/Select/internal/ClearIndicator/styled.ts
+++ src/components/Select/internal/ClearIndicator/styled.ts
@@ -1,13 +1,3 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  background-color: ${({ theme }) => theme.palette.gray.light};
-  border-radius: ${({ theme }) => theme.radius}px;
-  &:hover {
-    background-color: ${({ theme }) => theme.palette.gray.main};
-  }
-  & > div {
-    /* css-tlfecz-indicatorContainer */
-    padding: 4px;
-  }
-`;
+export const Container = styled.div``;
