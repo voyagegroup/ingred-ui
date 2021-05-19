84195cfe53b31d47c0da2b385e8ba3afd880b911
Yutaro Hayashi <40020812+youchann@users.noreply.github.com>
2021-05-19 12:43:42 +0900

    tweak

--- src/components/ClickAwayListener/ClickAwayListener.tsx
+++ src/components/ClickAwayListener/ClickAwayListener.tsx
@@ -11,15 +11,20 @@ const ClickAwayListener: React.FunctionComponent<ClickAwayListenerProps> = ({
   children,
 }) => {
   const childrenRef = React.useRef<HTMLElement>(null);
+  const bubbledEventTarget = React.useRef<EventTarget | null>(null);
 
   React.useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
+      console.log("target", event.target);
+      console.log("bubbled", bubbledEventTarget.current);
       if (
-        childrenRef.current != null &&
-        !childrenRef.current.contains(event.target as Node)
+        childrenRef.current == null ||
+        childrenRef.current.contains(event.target as Node) ||
+        bubbledEventTarget.current === event.target
       ) {
-        if (onClickAway) onClickAway(event);
+        return;
       }
+      if (onClickAway) onClickAway(event);
     };
     document.addEventListener("click", handleClickOutside);
     return () => {
@@ -27,9 +32,17 @@ const ClickAwayListener: React.FunctionComponent<ClickAwayListenerProps> = ({
     };
   }, [childrenRef, onClickAway]);
 
+  const handleBubbledEvents = (event: MouseEvent) => {
+    bubbledEventTarget.current = event.target;
+    if (children.props.onClick) {
+      children.props.onClick(event);
+    }
+  };
+
   const childrenProps = {
     ...children.props,
     ref: useMergeRefs(childrenRef, children.ref),
+    onClick: handleBubbledEvents,
   };
 
   return React.cloneElement(children, childrenProps);
--- testEnv/src/App.tsx
+++ testEnv/src/App.tsx
@@ -3,17 +3,34 @@ import {
   ThemeProvider,
   createTheme,
   Typography,
-  Spacer
+  Spacer,
+  ClickAwayListener
 } from 'ingred-ui';
 
 function App() {
   const theme = createTheme();
+  const [text, setText] = React.useState("not clicked");
+  const handleClickInner = () => {
+    console.log("clicked inner")
+    setText("clicked inner");
+  };
+  const handleClickOuter = () => {
+    console.log("clicked outer")
+    setText("clicked outer");
+  };
   return (
     <ThemeProvider theme={theme}>
       <Typography component="h1" size="xxxxxl" align="center">
         Create React App example
       </Typography>
-      <Spacer pt={2} />
+      <ClickAwayListener onClickAway={handleClickOuter}>
+        <div onClick={handleClickInner}>
+          Click inner/outer me!!
+          <div>hoehgoe</div>
+        </div>
+      </ClickAwayListener>
+      <div>{text}</div>
+     <Spacer pt={2} />
       <Typography color="secondary" size="xxl" align="center">
         Write some components in this environment.
       </Typography>
