d26040a93e697d039b3f85744e5103bd6cf12029
hirokikondo86 <hiroki_kondo@voyagegroup.com>
2021-08-03 11:06:08 +0900

    reafactor: restore FormRecipe

--- .storybook/documents/Information/CodeRecipes/FormRecipe.stories.tsx
+++ .storybook/documents/Information/CodeRecipes/FormRecipe.stories.tsx
@@ -13,8 +13,8 @@ import {
   Typography,
   ToggleButton,
   Button,
-  OptionType,
 } from "../../../../src/components";
+import { Controller, useForm, UnpackNestedValue } from "react-hook-form";
 
 export default {
   title: "Information/CodeRecipes/Form",
@@ -23,76 +23,30 @@ export default {
   },
 };
 
-type Values = {
+type FormType = {
   input: string;
   textField: string;
-  checkbox: string;
+  checkbox: boolean;
   datePicker: moment.Moment;
-  dateRangePicker: {
-    start: moment.Moment;
-    end: moment.Moment;
-  };
-  fileUploader: FileList | undefined;
-  radioButton: string | number | readonly string[] | null;
-  select: OptionType | undefined;
+  dateRangePicker: { start: moment.Moment; end: moment.Moment };
+  fileUploader: FileList;
+  radioButton: "1" | "2" | "3";
+  select: "1" | "2" | "3";
   toggleButton: boolean;
 };
 
 export const Overview: React.FC = () => {
-  const [values, setValues] = React.useState<Values>({
-    input: "hoge",
-    textField: "",
-    checkbox: "false",
-    datePicker: moment(),
-    dateRangePicker: { start: moment(), end: moment() },
-    fileUploader: undefined,
-    radioButton: null,
-    select: undefined,
-    toggleButton: true,
+  const { register, handleSubmit, control } = useForm<FormType>({
+    defaultValues: {
+      input: "hoge",
+      datePicker: moment(),
+      dateRangePicker: { start: moment(), end: moment() },
+      toggleButton: true,
+    },
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const target = e.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    setValues({ ...values, [name]: value });
-  };
-
-  const handleDateChange = (date: moment.Moment) => {
-    setValues({ ...values, datePicker: date });
-  };
-
-  const handleDatesChange = (dates: {
-    startDate: moment.Moment;
-    endDate: moment.Moment;
-  }) => {
-    setValues({
-      ...values,
-      dateRangePicker: { start: dates.startDate, end: dates.endDate },
-    });
-  };
-
-  const handleSelectFiles = (files: FileList) => {
-    setValues({
-      ...values,
-      fileUploader: files,
-    });
-  };
-
-  // TODO: 型をどうにかする
-  const handleSelectChange = (value: any) => {
-    setValues({
-      ...values,
-      select: { label: value?.label, value: value?.value },
-    });
-  };
-
-  const handleToggleButton = () => {
-    setValues((prev) => ({ ...values, toggleButton: !prev.toggleButton }));
-  };
-
-  const handleSubmit = () => {
-    alert(`submitted values \n ${JSON.stringify(values)}`);
+  const submit = (value: UnpackNestedValue<FormType>) => {
+    alert(`submitted values \n ${JSON.stringify(value)}`);
   };
 
   return (
@@ -100,48 +54,71 @@ export const Overview: React.FC = () => {
       <Typography component="h1" size="xxxxxl" weight="bold">
         Form
       </Typography>
+      <Typography>
+        We recommend to use{" "}
+        <a href="http://react-hook-form.com/">react-hook-form</a>. It&apos;s the
+        easiest way to implement form feature.
+        <br />
+        Here is examples. Source code is written in &rdquo;Story&rdquo; Tab at
+        footer.
+      </Typography>
       <Spacer pt={5} />
 
       {/* Input */}
       <Typography size="xxl">Input</Typography>
-      <Input value={values.input} name="input" onChange={handleInputChange} />
+      <Input ref={register} name="input" />
       <Spacer pt={2} />
 
       {/* TextField */}
       <Typography size="xxl">TextField</Typography>
-      <TextField
-        value={values.textField}
-        name="textField"
-        onChange={handleInputChange}
-      />
+      <TextField inputRef={register} name="textField" />
       <Spacer pt={2} />
 
       {/* Checkbox */}
       <Typography size="xxl">Checkbox</Typography>
-      <Checkbox
-        value={values.checkbox}
-        name="checkbox"
-        onChange={handleInputChange}
-      />
+      <Checkbox inputRef={register} name="checkbox" />
       <Spacer pt={2} />
 
       {/* DatePicker */}
       <Typography size="xxl">DatePicker</Typography>
-      <DatePicker date={values.datePicker} onDateChange={handleDateChange} />
+      <Controller
+        name="datePicker"
+        control={control}
+        render={(props) => (
+          <DatePicker
+            date={props.value}
+            onDateChange={(date) => props.onChange(date)}
+          />
+        )}
+      />
       <Spacer pt={2} />
 
       {/* DateRangePicker */}
       <Typography size="xxl">DateRangePicker</Typography>
-      <DateRangePicker
-        startDate={values.dateRangePicker.start}
-        endDate={values.dateRangePicker.end}
-        onDatesChange={handleDatesChange}
+      <Controller
+        name="dateRangePicker"
+        control={control}
+        render={(props) => (
+          <DateRangePicker
+            startDate={props.value.start}
+            endDate={props.value.end}
+            onDatesChange={(dates) =>
+              props.onChange({ start: dates.startDate, end: dates.endDate })
+            }
+          />
+        )}
       />
       <Spacer pt={2} />
 
       {/* FileUploader */}
       <Typography size="xxl">FileUploader</Typography>
-      <FileUploader onSelectFiles={(_e, files) => handleSelectFiles(files)} />
+      <Controller
+        name="fileUploader"
+        control={control}
+        render={(props) => (
+          <FileUploader onSelectFiles={(_e, files) => props.onChange(files)} />
+        )}
+      />
       <Spacer pt={2} />
 
       {/* RadioButton */}
@@ -149,9 +126,9 @@ export const Overview: React.FC = () => {
       {["1", "2", "3"].map((val) => (
         <RadioButton
           key={val}
+          inputRef={register}
           name="radioButton"
           value={val}
-          onChange={handleInputChange}
         >
           {val}
         </RadioButton>
@@ -160,22 +137,34 @@ export const Overview: React.FC = () => {
 
       {/* Select */}
       <Typography size="xxl">Select</Typography>
-      <Select
-        closeMenuOnSelect={true}
-        options={["1", "2", "3"].map((val) => ({ label: val, value: val }))}
-        onChange={handleSelectChange}
+      <Controller
+        name="select"
+        control={control}
+        render={(props) => (
+          <Select
+            closeMenuOnSelect={true}
+            options={["1", "2", "3"].map((val) => ({ label: val, value: val }))}
+            onChange={(value) => props.onChange(value)}
+          />
+        )}
       />
       <Spacer pt={2} />
 
       {/* ToggleButton */}
       <Typography size="xxl">ToggleButton</Typography>
-      <ToggleButton
-        active={values.toggleButton}
-        onChange={handleToggleButton}
+      <Controller
+        name="toggleButton"
+        control={control}
+        render={(props) => (
+          <ToggleButton
+            active={props.value}
+            onChange={() => props.onChange(!props.value)}
+          />
+        )}
       />
       <Spacer pt={2} />
 
-      <Button onClick={handleSubmit}>Submit</Button>
+      <Button onClick={handleSubmit(submit)}>Submit</Button>
     </>
   );
 };
