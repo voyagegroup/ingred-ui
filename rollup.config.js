888366566ac2c44cd6ec464e4acd8d08625adaec
Yutaro Hayashi <40020812+youchann@users.noreply.github.com>
2021-08-12 10:08:41 +0900

    update rollup plugins commonjs & node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from "@rollup/plugin-commonjs";
 import external from "rollup-plugin-peer-deps-external";
 import imagemin from "rollup-plugin-imagemin";
 import postcss from "rollup-plugin-postcss";
-import resolve from "rollup-plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import url from "rollup-plugin-url";
 import svgr from "@svgr/rollup";
 
@@ -40,18 +40,6 @@ export default {
       rollupCommonJSResolveHack: true,
       clean: true,
     }),
-    commonjs({
-      namedExports: {
-        "node_modules/react-dates/index.js": [
-          "DateRangePicker",
-          "SingleDatePicker",
-        ],
-        "node_modules/react-toast-notifications/dist/index.js": [
-          "useToasts",
-          "ToastProvider",
-          "DefaultToastContainer",
-        ],
-      },
-    }),
+    commonjs(),
   ],
 };
