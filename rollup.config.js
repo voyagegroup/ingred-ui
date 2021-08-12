14e8763d01b113523b6177d8ca667aae794c543b
Yutaro Hayashi <40020812+youchann@users.noreply.github.com>
2021-08-12 10:12:58 +0900

    update rollup plugin url

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import external from "rollup-plugin-peer-deps-external";
 import imagemin from "rollup-plugin-imagemin";
 import postcss from "rollup-plugin-postcss";
 import resolve from "@rollup/plugin-node-resolve";
-import url from "rollup-plugin-url";
+import url from "@rollup/plugin-url";
 import svgr from "@svgr/rollup";
 
 import pkg from "./package.json";
