fee6046d09dea3210777fd8997f161c6098a6441
小橋 大助 <daisuke_kohashi@voyagegroup.com>
2021-08-22 03:13:28 +0900

    add: locale selection on storybook

--- src/components/LocaleProvider/LocaleProvider.stories.tsx
+++ src/components/LocaleProvider/LocaleProvider.stories.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import { Story } from "@storybook/react/types-6-0";
 import LocaleProvider, { LocaleProviderProps } from ".";
-import { Button, ConfirmModal, ToggleButton } from "..";
+import { Button, ConfirmModal, Select, ToggleButton } from "..";
 
 import { jaJP } from "../../constants/locale";
+
+import * as locales from "../../constants/locale";
 
 export default {
   title: "Components/Data Display/LocaleProvider",
@@ -11,15 +13,29 @@ export default {
 };
 
 export const Example: Story<LocaleProviderProps> = (args) => {
+  const localeOptions = Object.keys(locales).map((_) => ({
+    label: _,
+    value: _,
+  }));
   const [active, setActive] = React.useState<boolean>(false);
   const [isOpen, setIsOpen] = React.useState(false);
+  const [localeOption, setLocaleOption] = React.useState(localeOptions[0]);
   const handleToggleButton = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLocationChange = (v: { label: string; value: string }) => {
+    setLocaleOption(v);
+  };
+
   return (
-    <LocaleProvider locale={jaJP}>
-      <div>Selected Locale: {args.locale} </div>
+    <LocaleProvider locale={locales[localeOption.value]}>
+      <div>Selected Locale: {localeOption.label} </div>
+      <Select
+        options={localeOptions}
+        defaultValue={localeOptions[0]}
+        onChange={handleLocationChange}
+      />
       <div>Toggle Button Example. </div>
       <ToggleButton active={active} onChange={() => setActive(!active)} />
 
