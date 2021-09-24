a531aaeb6f26843bd643d02dd873d3f187d7d8c1
youchann <yutaro_hayashi@voyagegroup.com>
2021-09-24 16:09:05 +0900

    Update ErrorText.tsx

--- src/components/ErrorText/ErrorText.tsx
+++ src/components/ErrorText/ErrorText.tsx
@@ -9,16 +9,22 @@ export type ErrorTextProps = {
   children: string;
 };
 
-const ErrorText: React.FunctionComponent<ErrorTextProps> = ({ children }) => {
-  const theme = useTheme();
-  return (
-    <Flex display="flex" alignItems="center">
-      <Spacer pr={0.5}>
-        <Icon name="exclamation" color={theme.palette.danger.main} size="md" />
-      </Spacer>
-      <Typography color={theme.palette.danger.main}>{children}</Typography>
-    </Flex>
-  );
-};
+const ErrorText = React.forwardRef<HTMLDivElement, ErrorTextProps>(
+  ({ children }, ref) => {
+    const theme = useTheme();
+    return (
+      <Flex ref={ref} display="flex" alignItems="center">
+        <Spacer pr={0.5}>
+          <Icon
+            name="exclamation"
+            color={theme.palette.danger.main}
+            size="md"
+          />
+        </Spacer>
+        <Typography color={theme.palette.danger.main}>{children}</Typography>
+      </Flex>
+    );
+  },
+);
 
 export default ErrorText;
