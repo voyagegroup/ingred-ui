538069871a6e98247d90e0c6248daf4cf68b2a2e
youchann <yutaro_hayashi@voyagegroup.com>
2021-09-24 12:06:56 +0900

    Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -7,34 +7,39 @@ export type CheckBoxProps = React.ComponentPropsWithoutRef<"input"> & {
   inputRef?: React.Ref<HTMLInputElement>;
 };
 
-const Checkbox: React.FunctionComponent<CheckBoxProps> = ({
-  children,
-  indeterminate = false,
-  error = false,
-  disabled = false,
-  inputRef,
-  ...rest
-}) => {
-  return (
-    <Styled.Container disabled={disabled}>
-      <Styled.Checkbox
-        ref={inputRef}
-        error={error}
-        readOnly={true}
-        type="checkbox"
-        indeterminate={indeterminate}
-        disabled={disabled}
-        {...rest}
-      />
-      <Styled.Span
-        error={error}
-        indeterminate={indeterminate}
-        hasChild={!!children}
-      >
-        {children}
-      </Styled.Span>
-    </Styled.Container>
-  );
-};
+const Checkbox = React.forwardRef<HTMLLabelElement, CheckBoxProps>(
+  (
+    {
+      children,
+      indeterminate = false,
+      error = false,
+      disabled = false,
+      inputRef,
+      ...rest
+    },
+    ref,
+  ) => {
+    return (
+      <Styled.Container ref={ref} disabled={disabled}>
+        <Styled.Checkbox
+          ref={inputRef}
+          error={error}
+          readOnly={true}
+          type="checkbox"
+          indeterminate={indeterminate}
+          disabled={disabled}
+          {...rest}
+        />
+        <Styled.Span
+          error={error}
+          indeterminate={indeterminate}
+          hasChild={!!children}
+        >
+          {children}
+        </Styled.Span>
+      </Styled.Container>
+    );
+  },
+);
 
 export default Checkbox;
