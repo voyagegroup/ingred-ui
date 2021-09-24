876a7d562a0333f3c223b96e2dd8d0a5fb5f6f13
youchann <yutaro_hayashi@voyagegroup.com>
2021-09-24 18:38:50 +0900

    Update ToggleButton.tsx

--- src/components/ToggleButton/ToggleButton.tsx
+++ src/components/ToggleButton/ToggleButton.tsx
@@ -13,55 +13,57 @@ export type ToggleButtonProps = {
   inputRef?: React.Ref<HTMLInputElement>;
 };
 
-const ToggleButton: React.FunctionComponent<ToggleButtonProps> = (inProps) => {
-  const props = useLocaleProps({ props: inProps, name: "ToggleButton" });
-  const {
-    active = false,
-    disabled = false,
-    onChange,
-    width = "56px",
-    activeText = "ON",
-    inActiveText = "OFF",
-    inputRef,
-  } = props;
-  return (
-    <Styled.Container width={width}>
-      <Styled.Label active={active} disabled={disabled} width={width}>
-        <Styled.HiddenInput
-          ref={inputRef}
-          checked={active}
-          type="checkbox"
-          // eslint-disable-next-line react/jsx-handler-names
-          readOnly={onChange == undefined}
-          disabled={disabled}
-          onChange={onChange}
-        />
-        <Styled.ActiveLabelText>
-          <Typography
-            component="div"
-            color={disabled ? "disabled" : "primary"}
-            align="left"
-            size="xs"
-            weight="bold"
-          >
-            {activeText}
-          </Typography>
-        </Styled.ActiveLabelText>
-        <Styled.InActiveLabelText>
-          <Typography
-            component="div"
-            color={disabled ? "disabled" : "secondary"}
-            align="right"
-            size="xs"
-            weight="bold"
-          >
-            {inActiveText}
-          </Typography>
-        </Styled.InActiveLabelText>
-        <Styled.ToggleButton active={active} disabled={disabled} />
-      </Styled.Label>
-    </Styled.Container>
-  );
-};
+const ToggleButton = React.forwardRef<HTMLDivElement, ToggleButtonProps>(
+  (inProps, ref) => {
+    const props = useLocaleProps({ props: inProps, name: "ToggleButton" });
+    const {
+      active = false,
+      disabled = false,
+      onChange,
+      width = "56px",
+      activeText = "ON",
+      inActiveText = "OFF",
+      inputRef,
+    } = props;
+    return (
+      <Styled.Container ref={ref} width={width}>
+        <Styled.Label active={active} disabled={disabled} width={width}>
+          <Styled.HiddenInput
+            ref={inputRef}
+            checked={active}
+            type="checkbox"
+            // eslint-disable-next-line react/jsx-handler-names
+            readOnly={onChange == undefined}
+            disabled={disabled}
+            onChange={onChange}
+          />
+          <Styled.ActiveLabelText>
+            <Typography
+              component="div"
+              color={disabled ? "disabled" : "primary"}
+              align="left"
+              size="xs"
+              weight="bold"
+            >
+              {activeText}
+            </Typography>
+          </Styled.ActiveLabelText>
+          <Styled.InActiveLabelText>
+            <Typography
+              component="div"
+              color={disabled ? "disabled" : "secondary"}
+              align="right"
+              size="xs"
+              weight="bold"
+            >
+              {inActiveText}
+            </Typography>
+          </Styled.InActiveLabelText>
+          <Styled.ToggleButton active={active} disabled={disabled} />
+        </Styled.Label>
+      </Styled.Container>
+    );
+  },
+);
 
 export default ToggleButton;
