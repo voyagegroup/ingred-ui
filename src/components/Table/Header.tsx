c2b631a80d5e9cec7ef10f1d34dd575b17785791
takurinton <takurinton@takurinton.com>
2021-10-21 15:41:30 +0900

    add: Table Header Props

--- src/components/Table/Header.tsx
+++ src/components/Table/Header.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 
-export type Props = {};
+export type Props = {
+  children: React.ReactNode;
+};
 
 export const Header = React.forwardRef<HTMLTableSectionElement, Props>(
   ({ children }, ref) => <thead ref={ref}>{children}</thead>,
