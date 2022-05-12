e2d1bd2821f473639b78a6e394e699ecc45baf13
horiy0125 <kh1520.shsf@gmail.com>
2022-05-12 14:24:21 +0900

    fix: add semi-colon

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,6 @@
 export { Radius } from "./radius";
 export { Space } from "./space";
 export { BreakPoint } from "./breakPoint";
-export { colors } from "./color"
+export { colors } from "./color";
 export type { Color } from "./color";
 export type { Depth } from "./depth";
