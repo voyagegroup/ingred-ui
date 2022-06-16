3b7f3994c2da14cb96db9964456c44fb41216161
youchann <yutaro_hayashi@voyagegroup.com>
2022-06-16 18:20:54 +0900

    パッケージいじり

--- testEnv/src/App.tsx
+++ testEnv/src/App.tsx
@@ -4,23 +4,26 @@ import {
   Typography,
   Spacer
 } from 'ingred-ui';
+import { StrictMode } from 'react';
 
 function App() {
   const theme = createTheme();
   return (
-    <ThemeProvider theme={theme}>
-      <Typography component="h1" size="xxxxxl" align="center">
-        Create React App example
-      </Typography>
-      <Spacer pt={2} />
-      <Typography color="secondary" size="xxl" align="center">
-        Write some components in this environment.
-      </Typography>
-      <Spacer pt={2} />
-      <Typography color="secondary" size="xxl" align="center">
-        Copyright © <a href="https://ingred-ui.netlify.app/">Your Website</a> 2020.
-      </Typography>
-    </ThemeProvider>
+    <StrictMode>
+      <ThemeProvider theme={theme}>
+        <Typography component="h1" size="xxxxxl" align="center">
+          Create React App example
+        </Typography>
+        <Spacer pt={2} />
+        <Typography color="secondary" size="xxl" align="center">
+          Write some components in this environment.
+        </Typography>
+        <Spacer pt={2} />
+        <Typography color="secondary" size="xxl" align="center">
+          Copyright © <a href="https://ingred-ui.netlify.app/">Your Website</a> 2020.
+        </Typography>
+      </ThemeProvider>
+    </StrictMode>
   );
 }
 
