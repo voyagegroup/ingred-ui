c780cf4b1435c3f5e4a40921d2c60eb55d945e16
takurinton <takurinton@takurinton.dev>
2022-07-14 16:20:40 +0900

    fix: separate component to createChainedFunction

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as Styled from "./styled";
 import { Property } from "csstype";
+import { createChainedFunction } from "../../utils/createChainedFunction";
 
 export type InputProps = (
   | React.ComponentPropsWithoutRef<"input">
@@ -16,25 +17,14 @@ const Input = React.forwardRef<
   HTMLTextAreaElement | HTMLInputElement,
   InputProps
 >(({ error = false, multiline = false, resize = "both", ...rest }, ref) => {
-  if (rest.type === "number") {
-    const onWheel = (
-      event: React.WheelEvent<HTMLInputElement> &
-        React.WheelEvent<HTMLTextAreaElement>,
-    ) => {
-      event.currentTarget.blur();
-      if (rest.onWheel) rest.onWheel(event);
-    };
-    return (
-      <Styled.Input
-        {...rest}
-        ref={ref as any}
-        as={(multiline ? "textarea" : "input") as any}
-        isError={error}
-        resize={resize}
-        onWheel={onWheel}
-      />
-    );
-  }
+  const handleWheel = (
+    event: React.WheelEvent<HTMLInputElement> &
+      React.WheelEvent<HTMLTextAreaElement>,
+  ) => {
+    event.currentTarget.blur();
+    if (rest.onWheel) rest.onWheel(event);
+  };
+
   return (
     <Styled.Input
       {...rest}
@@ -42,6 +32,10 @@ const Input = React.forwardRef<
       as={(multiline ? "textarea" : "input") as any}
       isError={error}
       resize={resize}
+      onWheel={createChainedFunction(
+        rest.type === "number" ? handleWheel : null,
+        rest.onWheel ? rest.onWheel : null,
+      )}
     />
   );
 });
