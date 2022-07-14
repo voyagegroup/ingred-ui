6dbc3b31dc1d09154991e12df8cfa786f1003c8e
takurinton <takurinton@takurinton.dev>
2022-07-14 12:14:57 +0900

    add: inputRef

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as Styled from "./styled";
 import { Property } from "csstype";
+import { useMergeRefs } from "../../hooks/useMergeRefs";
 
 export type InputProps = (
   | React.ComponentPropsWithoutRef<"input">
@@ -12,52 +13,60 @@ export type InputProps = (
   resize?: Property.Resize;
 };
 
-const isNumber = (n: string) => {
-  // empty or white space
-  if (n === "" || n === " ") {
-    return false;
-  }
-  return !isNaN(Number(n));
-};
+const NumberInput = React.forwardRef<
+  HTMLTextAreaElement | HTMLInputElement,
+  InputProps
+>(({ error = false, multiline = false, resize = "both", ...rest }, ref) => {
+  const inputRef = React.useRef<HTMLInputElement | HTMLTextAreaElement>(null);
+  const mergeedRef = useMergeRefs(ref, inputRef);
+
+  const onChange = React.useCallback(
+    (
+      event: React.ChangeEvent<HTMLInputElement> &
+        React.ChangeEvent<HTMLTextAreaElement>,
+    ) => {
+      const value = event.target.value;
+      if (isNaN(Number(value)) || value === " ") {
+        if (inputRef.current) {
+          inputRef.current.value = value.substring(0, value.length - 1);
+        }
+        return;
+      }
+      if (rest.onChange) rest.onChange(event);
+    },
+    [rest],
+  );
+
+  const onKeyDown = React.useCallback(
+    (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === " ") {
+        event.preventDefault();
+      }
+    },
+    [],
+  );
+
+  return (
+    <Styled.Input
+      {...rest}
+      ref={mergeedRef}
+      type="text"
+      as={(multiline ? "textarea" : "input") as any}
+      isError={error}
+      resize={resize}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+    />
+  );
+});
 
 const Input = React.forwardRef<
   HTMLTextAreaElement | HTMLInputElement,
   InputProps
->(({ error = false, multiline = false, resize = "both", ...rest }, ref) => {
-  if (rest.type === "number") {
-    const onChange = (
-      event: React.ChangeEvent<HTMLInputElement> &
-        React.ChangeEvent<HTMLTextAreaElement>,
-    ) => {
-      if (!isNumber(event.target.value)) return;
-      if (rest.onChange) rest.onChange(event);
-    };
-
-    const onKeyDown = (
-      event: React.KeyboardEvent<HTMLInputElement> &
-        React.KeyboardEvent<HTMLTextAreaElement>,
-    ) => {
-      if (!isNumber(event.key) && !(event.key === "Backspace")) {
-        event.preventDefault();
-      }
-      if (rest.onKeyDown) rest.onKeyDown(event);
-    };
-
-    return (
-      <Styled.Input
-        {...rest}
-        ref={ref as any}
-        type="text"
-        as={(multiline ? "textarea" : "input") as any}
-        isError={error}
-        resize={resize}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-      />
-    );
-  }
-
-  return (
+>(({ error = false, multiline = false, resize = "both", ...rest }, ref) =>
+  rest.type === "number" ? (
+    <NumberInput {...rest} ref={ref} />
+  ) : (
     <Styled.Input
       {...rest}
       ref={ref as any}
@@ -65,7 +74,7 @@ const Input = React.forwardRef<
       isError={error}
       resize={resize}
     />
-  );
-});
+  ),
+);
 
 export default Input;
