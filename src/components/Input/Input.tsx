25c0601de3445f178267bccaa0b6553c10a183f9
takurinton <takurinton@takurinton.dev>
2022-07-14 14:49:17 +0900

    remove: NumberInput and add onWheel

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as Styled from "./styled";
 import { Property } from "csstype";
-import { useMergeRefs } from "../../hooks/useMergeRefs";
 
 export type InputProps = (
   | React.ComponentPropsWithoutRef<"input">
@@ -13,60 +12,30 @@ export type InputProps = (
   resize?: Property.Resize;
 };
 
-const NumberInput = React.forwardRef<
-  HTMLTextAreaElement | HTMLInputElement,
-  InputProps
->(({ error = false, multiline = false, resize = "both", ...rest }, ref) => {
-  const inputRef = React.useRef<HTMLInputElement | HTMLTextAreaElement>(null);
-  const mergeedRef = useMergeRefs(ref, inputRef);
-
-  const onChange = React.useCallback(
-    (
-      event: React.ChangeEvent<HTMLInputElement> &
-        React.ChangeEvent<HTMLTextAreaElement>,
-    ) => {
-      const value = event.target.value;
-      if (isNaN(Number(value)) || value === " ") {
-        if (inputRef.current) {
-          inputRef.current.value = value.substring(0, value.length - 1);
-        }
-        return;
-      }
-      if (rest.onChange) rest.onChange(event);
-    },
-    [rest],
-  );
-
-  const onKeyDown = React.useCallback(
-    (event: React.KeyboardEvent<HTMLInputElement>) => {
-      if (event.key === " ") {
-        event.preventDefault();
-      }
-    },
-    [],
-  );
-
-  return (
-    <Styled.Input
-      {...rest}
-      ref={mergeedRef}
-      type="text"
-      as={(multiline ? "textarea" : "input") as any}
-      isError={error}
-      resize={resize}
-      onChange={onChange}
-      onKeyDown={onKeyDown}
-    />
-  );
-});
-
 const Input = React.forwardRef<
   HTMLTextAreaElement | HTMLInputElement,
   InputProps
->(({ error = false, multiline = false, resize = "both", ...rest }, ref) =>
-  rest.type === "number" ? (
-    <NumberInput {...rest} ref={ref} />
-  ) : (
+>(({ error = false, multiline = false, resize = "both", ...rest }, ref) => {
+  if (rest.type === "number") {
+    const onWheel = (
+      event: React.WheelEvent<HTMLInputElement> &
+        React.WheelEvent<HTMLTextAreaElement>,
+    ) => {
+      event.currentTarget.blur();
+      if (rest.onWheel) rest.onWheel(event);
+    };
+    return (
+      <Styled.Input
+        {...rest}
+        ref={ref as any}
+        as={(multiline ? "textarea" : "input") as any}
+        isError={error}
+        resize={resize}
+        onWheel={onWheel}
+      />
+    );
+  }
+  return (
     <Styled.Input
       {...rest}
       ref={ref as any}
@@ -74,7 +43,7 @@ const Input = React.forwardRef<
       isError={error}
       resize={resize}
     />
-  ),
-);
+  );
+});
 
 export default Input;
