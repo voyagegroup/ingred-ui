1056942ba364d700f867fc91ef7bd64977d4682c
keisukeonishi <ca01249@cartahd.com>
2022-08-18 14:46:42 +0900

    shadowsをkeyvalueの形で持つ

--- src/styles/shadows.ts
+++ src/styles/shadows.ts
@@ -1,14 +1,6 @@
-export type ShadowsType = string[];
+export type ShadowsType = { [key: string]: string };
 
-// 参照方法は theme.shadows[0] で "none" が取得できるようになる
-export const Shadows: ShadowsType = [
-  "none",
+export const Shadows: ShadowsType = {
+  none: "none",
   // and more...
-];
-
-// key-value で持つのもありかもしれない
-// 参照方法は theme.shadows.none で "none" が取得できるようになる
-// export const ShadowsMap: { [key: string]: string } = {
-//   none: "none",
-//   // and more...
-// };
+};
