0e976f0b97cd5602d0206ac39cfd39c7faab8fc1
takurinton <takurinton@takurinton.dev>
2022-08-23 09:01:19 +0900

    ...

--- src/utils/getColorWithOpacity.ts
+++ src/utils/getColorWithOpacity.ts
@@ -1,14 +1,10 @@
-import { colors } from "../styles";
 import { hexToRgba } from "./hexToRgba";
 
 export const OPACITY_BASE = 8;
 export const opacityBase = OPACITY_BASE / 100;
 
 // opacity と 色から rgba を作る
-export const getColorWithOpacity = (
-  opacity = 0,
-  color = colors ? colors.basic[900] : "#041C33",
-) => {
+export const getColorWithOpacity = (opacity = 0, color = "#041C33") => {
   if (opacity === 0) {
     return "transparent";
   }
