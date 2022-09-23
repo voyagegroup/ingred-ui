ce08cb38111f4c3b0b75b01564f98ce456ce82a2
penicillin0 <ayahitonakamura@gmail.com>
2022-09-23 16:21:30 +0900

    mdx -> tsx

---
+++ src/components/ActionButton/ActionButton.stories.tsx
@@ -0,0 +1,55 @@
+import { action } from "@storybook/addon-actions";
+import { ArgsTable, Description, Stories, Title } from "@storybook/addon-docs";
+import { Story } from "@storybook/react/types-6-0";
+import React from "react";
+import { Flex, Spacer, Typography } from "..";
+import ActionButton, { ActionButtonProps } from "./";
+
+export default {
+  title: "Components/Inputs/ActionButton",
+  component: ActionButton,
+  args: {
+    onClick: action("clicked"),
+  },
+  parameters: {
+    docs: {
+      source: { type: "code" },
+      page: () => (
+        <>
+          <Title />
+          <Description markdown={"It can behave like a `<button />` tags."} />
+          <ArgsTable of={ActionButton} />
+          <Stories includePrimary title="Stories" />
+        </>
+      ),
+    },
+  },
+};
+
+export const DesignSamples: Story<ActionButtonProps> = () => {
+  return (
+    <Flex display="flex">
+      <Spacer mr={5}>
+        <Typography weight="bold">Primary</Typography>
+        <Spacer pt={1} />
+        <ActionButton icon="pencil" color="primary">
+          Edit
+        </ActionButton>
+      </Spacer>
+      <Spacer mr={5}>
+        <Typography weight="bold">Warning</Typography>
+        <Spacer pt={1} />
+        <ActionButton icon="pencil" color="warning">
+          Edit
+        </ActionButton>
+      </Spacer>
+      <Spacer mr={5}>
+        <Typography weight="bold">Disabled</Typography>
+        <Spacer pt={1} />
+        <ActionButton icon="pencil" disabled={true}>
+          Edit
+        </ActionButton>
+      </Spacer>
+    </Flex>
+  );
+};
