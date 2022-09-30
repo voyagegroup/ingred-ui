c58410e7d12d61148a3b0ef2356fd729e52c720b
Nakamura Ayahito <56141035+penicillin0@users.noreply.github.com>
2022-09-30 10:12:00 +0900

    【Storybook】ActionButton、mdxをtsxに変更 (#1000)
    * mdx -> tsx
    
    * use flex gap
    
    * rm props type
    
    * props 共通化
    
    * separate story
    
    * change use Template.bind
    
    * Update src/components/ActionButton/ActionButton.stories.tsx
    
    Co-authored-by: takurinton <takurinton@takurinton.dev>

---
+++ src/components/ActionButton/ActionButton.stories.tsx
@@ -0,0 +1,42 @@
+import { action } from "@storybook/addon-actions";
+import { ArgsTable, Description, Stories, Title } from "@storybook/addon-docs";
+import { Story } from "@storybook/react/types-6-0";
+import React from "react";
+import ActionButton, { ActionButtonProps } from "./";
+
+export default {
+  title: "Components/Inputs/ActionButton",
+  component: ActionButton,
+  args: {
+    onClick: action("clicked"),
+    icon: "pencil",
+    children: "Edit",
+  },
+  parameters: {
+    docs: {
+      source: { type: "code" },
+      page: () => (
+        <>
+          <Title />
+          <Description markdown={"It can behave like a `<button />` tags."} />
+          <ArgsTable of={ActionButton} />
+          <Stories includePrimary title="Stories" />
+        </>
+      ),
+    },
+  },
+};
+
+const Template: Story<ActionButtonProps> = (args) => <ActionButton {...args} />;
+
+export const Primary = Template.bind({});
+
+export const Warning = Template.bind({});
+Warning.args = {
+  color: "warning",
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
