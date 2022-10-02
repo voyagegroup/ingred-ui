c18b9f5758487029edf888fff844ce805a368399
penicillin0 <ayahitonakamura@gmail.com>
2022-10-02 16:21:54 +0900

    tweak

--- src/components/Slide/Slide.stories.tsx
+++ src/components/Slide/Slide.stories.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Title, Description, ArgsTable, Stories } from "@storybook/addon-docs";
 import Slide from "./";
 import Flex from "../Flex";
