99bd3ccdaf061c3782b8674b28a3d2346a76a232
ca01655 <ca01655@M-Q26CXQW0CJ.voyagegroup.local>
2022-10-03 15:22:02 +0900

    mdx to tsx

---
+++ src/components/Spinner/Spinner.stories.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Story } from "@storybook/react/types-6-0";
+import { Title, ArgsTable, Stories } from "@storybook/addon-docs";
+import Spinner , { SpinnerProps } from "./Spinner";
+
+export default {
+  title: "Components/Feedback/Spinner",
+  component: Spinner,
+  parameters: {
+    docs: {
+      source: { type: "code" },
+      page: () => (
+        <>
+          <Title />
+          <ArgsTable of={Spinner} />
+          <Stories includePrimary title="Stories" />
+        </>
+      ),
+    },
+  },
+};
+
+export const Example: Story<SpinnerProps> = (args) => <Spinner {...args} />;
