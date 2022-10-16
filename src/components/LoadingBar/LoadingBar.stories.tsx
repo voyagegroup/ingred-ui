2cce0393f4b70414bd5e44f05d6ff23062bc2e12
penicillin0 <ayahitonakamura@gmail.com>
2022-10-16 20:07:36 +0900

    mdx -> tsx

---
+++ src/components/LoadingBar/LoadingBar.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Title, ArgsTable, Stories } from "@storybook/addon-docs";
+import LoadingBar from "./LoadingBar";
+import { ComponentStory } from "@storybook/react";
+
+export default {
+  title: "Components/Feedback/LoadingBar",
+  component: LoadingBar,
+  parameters: {
+    docs: {
+      source: { type: "code" },
+      page: () => (
+        <>
+          <Title />
+          <ArgsTable of={LoadingBar} />
+          <Stories includePrimary title="Stories" />
+        </>
+      ),
+    },
+  },
+};
+
+export const Example: ComponentStory<typeof LoadingBar> = (args) => (
+  <div style={{ padding: "24px", backgroundColor: "silver" }}>
+    <LoadingBar {...args} />
+  </div>
+);
