85a4217d8fa0eeaf77d62d29685d21772c00f513
takurinton <takurinton@takurinton.dev>
2022-10-19 07:09:41 +0900

    init

---
+++ testEnv/ssr-react/src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ThemeProvider, createTheme, Typography, Spacer } from "ingred-ui";
+
+export default function Home() {
+  const theme = createTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <Typography component="h1" size="xxxxxl" align="center">
+        Create React App example
+      </Typography>
+      <Spacer pt={2} />
+      <Typography color="secondary" size="xxl" align="center">
+        Write some components in this environment.
+      </Typography>
+      <Spacer pt={2} />
+      <Typography color="secondary" size="xxl" align="center">
+        Copyright © <a href="https://ingred-ui.netlify.app/">Your Website</a>{" "}
+        2022.
+      </Typography>
+    </ThemeProvider>
+  );
+}
