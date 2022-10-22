03f992a0bc8c7f042c1327d2440d9c4547392f57
penicillin0 <ayahitonakamura@gmail.com>
2022-10-22 22:34:19 +0900

    共通化

--- src/components/ConfirmModal/ConfirmModal.stories.tsx
+++ src/components/ConfirmModal/ConfirmModal.stories.tsx
@@ -29,7 +29,7 @@ export default {
   },
 };
 
-export const Basic: Story<ConfirmModalProps> = (args) => {
+const Template: Story<ConfirmModalProps> = (args) => {
   const [isOpen, setIsOpen] = React.useState(args.isOpen);
   const handleToggleButton = () => {
     setIsOpen(!isOpen);
@@ -37,164 +37,85 @@ export const Basic: Story<ConfirmModalProps> = (args) => {
   return (
     <>
       <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        onSubmit={action("submitted")}
-        onClose={handleToggleButton}
-        {...args}
-        isOpen={isOpen}
-      >
-        Content
+      <ConfirmModal onClose={handleToggleButton} {...args} isOpen={isOpen}>
+        {args.children}
       </ConfirmModal>
     </>
   );
 };
 
-export const FullSize: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        fullSize={true}
-        onSubmit={action("submitted")}
-        onClose={handleToggleButton}
-        {...args}
-        isOpen={isOpen}
-      >
-        Content
-      </ConfirmModal>
-    </>
-  );
+export const Basic = Template.bind({});
+Basic.args = {
+  children: "Content",
+  onSubmit: action("submitted"),
 };
 
-export const WithTips: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        tipElement={<div>Description for this modal.</div>}
-        onSubmit={action("submitted")}
-        onClose={handleToggleButton}
-        {...args}
-        isOpen={isOpen}
-      >
-        Content
-      </ConfirmModal>
-    </>
-  );
+export const FullSize = Template.bind({});
+FullSize.args = {
+  children: "Content",
+  onSubmit: action("submitted"),
+  fullSize: true,
 };
 
-export const WithSubactions: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
+export const WithTips = Template.bind({});
+WithTips.args = {
+  children: "Content",
+  onSubmit: action("submitted"),
+  tipElement: <div>Description for this modal.</div>,
+};
+
+export const WithSubActions = Template.bind({});
+WithSubActions.args = {
+  children: "Content",
+  onSubmit: action("submitted"),
+  fullSize: true,
+  subActions: [
+    {
+      title: "Download in CSV format",
+      icon: "export",
+      action: action(`clicked "Download in CSV format"`),
+    },
+    {
+      title: "Add to dashboard",
+      icon: "export",
+      action: action(`clicked "Add to dashboard"`),
+    },
+  ],
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  children: "Content",
+  onSubmit: () => {},
+  loading: true,
+};
+
+export const OverflowYScroll = Template.bind({});
+OverflowYScroll.args = {
+  children: (
     <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        onClose={handleToggleButton}
-        onSubmit={action("submitted")}
-        {...args}
-        fullSize={true}
-        subActions={[
+      <Spacer my={3} />
+      <DataTable
+        data={data}
+        columns={[
           {
-            title: "Download in CSV format",
-            icon: "export",
-            action: () => {},
+            name: "ID",
+            selector: (data) => data.id,
           },
           {
-            title: "Add to dashboard",
-            icon: "export",
-            action: () => {},
+            name: "name",
+            selector: (data) => data.date,
           },
         ]}
-        isOpen={isOpen}
-      >
-        Content
-      </ConfirmModal>
+      />
     </>
-  );
+  ),
+  onSubmit: action("submitted"),
+  overflowYScroll: true,
 };
 
-export const Loading: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        loading={true}
-        onClose={handleToggleButton}
-        {...args}
-        isOpen={isOpen}
-        onSubmit={() => {}}
-      >
-        Content
-      </ConfirmModal>
-    </>
-  );
-};
-
-export const OverflowYScroll: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        overflowYScroll={true}
-        onClose={handleToggleButton}
-        onSubmit={action("submitted")}
-        {...args}
-        isOpen={isOpen}
-      >
-        <Spacer my={2} />
-        <DataTable
-          data={data}
-          columns={[
-            {
-              name: "ID",
-              selector: (data) => data.id,
-            },
-            {
-              name: "name",
-              selector: (data) => data.date,
-            },
-          ]}
-        />
-      </ConfirmModal>
-    </>
-  );
-};
-
-export const WithoutFooter: Story<ConfirmModalProps> = (args) => {
-  const [isOpen, setIsOpen] = React.useState(args.isOpen);
-  const handleToggleButton = () => {
-    setIsOpen(!isOpen);
-  };
-  return (
-    <>
-      <Button onClick={handleToggleButton}>Open Modal</Button>
-      <ConfirmModal
-        onClose={handleToggleButton}
-        onSubmit={undefined}
-        {...args}
-        isOpen={isOpen}
-      >
-        Content
-      </ConfirmModal>
-    </>
-  );
+export const WithoutFooter = Template.bind({});
+WithoutFooter.args = {
+  children: "Content",
+  onSubmit: undefined,
 };
