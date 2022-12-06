a96cf0b6a1bfb5d724c8bffd3e4839a3d598e6f0
maktak1995 <takehiro_makita@voyagegroup.com>
2022-12-06 13:50:38 +0900

    キーボード入力の振る舞いを追加

--- src/components/CalenderInput/CalenderInput.tsx
+++ src/components/CalenderInput/CalenderInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import * as Styled from "./styled";
 import dayjs from "dayjs";
 
@@ -8,40 +8,71 @@ const CalenderInput: React.FC = () => {
   const [month, setMonth] = useState<number>(today.month() + 1);
   const [date, setDate] = useState<number>(today.date());
 
-  const handleYearChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setYear(Number(e.target.value));
+  const yearInput = useRef<HTMLInputElement>(null);
+  const monthInput = useRef<HTMLInputElement>(null);
+  const dateInput = useRef<HTMLInputElement>(null);
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setYear(Number(event.target.value));
   };
-  const handleMonthChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setMonth(Number(e.target.value));
+  const handleMonthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMonth(Number(event.target.value));
   };
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setDate(Number(e.target.value));
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(Number(event.target.value));
+  };
+
+  const handleYearKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" || event.key === "ArrowRight")
+      monthInput.current?.focus();
+    if (event.key === "ArrowUp") setYear(year + 1);
+    if (event.key === "ArrowDown") setYear(year - 1);
+  };
+  const handleMonthKeyPress = (
+    event: React.KeyboardEvent<HTMLInputElement>,
+  ) => {
+    if (event.key === "Enter" || event.key === "ArrowRight")
+      dateInput.current?.focus();
+    if (event.key === "ArrowLeft") yearInput.current?.focus();
+    if (event.key === "ArrowUp" && month < 12) setMonth(month + 1);
+    if (event.key === "ArrowDown" && month > 1) setMonth(month - 1);
+  };
+  const handleDateKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "ArrowLeft") monthInput.current?.focus();
+    if (event.key === "ArrowUp" && date < 31) setDate(date + 1);
+    if (event.key === "ArrowDown" && date > 1) setDate(date - 1);
   };
 
   return (
     <Styled.Container>
       <Styled.Input
+        ref={yearInput}
         letterCount={4}
         type={"text"}
         aria-label="Year"
         value={year}
         onChange={handleYearChange}
+        onKeyDown={handleYearKeyPress}
       />
       /
       <Styled.Input
+        ref={monthInput}
         letterCount={2}
         type={"text"}
         aria-label="Month"
         value={("00" + month).slice(-2)}
         onChange={handleMonthChange}
+        onKeyDown={handleMonthKeyPress}
       />
       /
       <Styled.Input
+        ref={dateInput}
         letterCount={2}
         type={"text"}
         aria-label="Date"
         value={("00" + date).slice(-2)}
         onChange={handleDateChange}
+        onKeyDown={handleDateKeyPress}
       />
     </Styled.Container>
   );
