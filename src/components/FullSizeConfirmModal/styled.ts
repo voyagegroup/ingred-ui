1c3508e68f0593bcbc8c41d86d5b736fba255947
penicillin0 <ayahitonakamura@gmail.com>
2023-01-30 23:13:17 +0900

    rm no use style

--- src/components/FullSizeConfirmModal/styled.ts
+++ src/components/FullSizeConfirmModal/styled.ts
@@ -2,25 +2,6 @@ import styled, { keyframes, css } from "styled-components";
 import { addScrollbarProperties } from "../../utils/scrollbar";
 import Card from "../Card";
 
-const fadeIn = keyframes`
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-`;
-export const ModalBackground = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  /* TODO: color */
-  background-color: #001326cb;
-  animation: ${fadeIn} 0.4s;
-`;
-
 const FullSizeSlideIn = keyframes`
   0% {
     transform: translate(-50%, 0%);
