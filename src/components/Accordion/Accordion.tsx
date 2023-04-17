e163f8a512d0fa8e31138197a7300b16942a759a
takurinton <takurinton@takurinton.dev>
2023-04-17 10:27:24 +0900

    feat `<Accordion />` component (#1225)

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,91 @@
+import React, {
+  ReactNode,
+  forwardRef,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { useTheme } from "../../themes";
+import Icon from "../Icon";
+import * as Styled from "./styled";
+
+export type AccordionProps = {
+  title: ReactNode;
+  expanded?: boolean;
+  disabled?: boolean;
+  onChange?: (event: React.SyntheticEvent, expanded: boolean) => void;
+  children: ReactNode | ReactNode[];
+};
+
+const Accordion = forwardRef<HTMLDivElement, AccordionProps>(function Accordion(
+  { title, expanded = false, disabled = false, onChange, children, ...rest },
+  ref,
+) {
+  const theme = useTheme();
+  const [expandedState, setExpandedState] = useState(expanded);
+  const accordionContentContainerRef = useRef<HTMLDivElement>(null);
+
+  const height = useMemo(() => {
+    if (expandedState && accordionContentContainerRef.current) {
+      return accordionContentContainerRef.current.scrollHeight;
+    }
+    return 0;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [expandedState, accordionContentContainerRef.current]);
+
+  const handleChange = (event: React.SyntheticEvent, expanded: boolean) => {
+    setExpandedState(expanded);
+    onChange && onChange(event, expanded);
+  };
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    handleChange(event, !expandedState);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "Enter") {
+      handleChange(event, !expandedState);
+    }
+  };
+
+  useEffect(() => {
+    setExpandedState(expanded);
+  }, [expanded]);
+
+  return (
+    <div ref={ref} {...rest}>
+      <Styled.AccordionTitle
+        display="flex"
+        tabIndex={disabled ? -1 : 0}
+        role="button"
+        aria-expanded={expandedState}
+        aria-controls={`accordion-content-${title}`}
+        expanded={expandedState}
+        disabled={disabled}
+        onClick={handleClick}
+        onKeyDown={handleKeyDown}
+      >
+        <Styled.AccordionTitleChildren>{title}</Styled.AccordionTitleChildren>
+        <Styled.DropdownIndicator>
+          <Styled.IconButton expanded={expandedState}>
+            <Icon
+              name="arrow_bottom"
+              size="md"
+              color={disabled ? theme.palette.text.disabled : "black"}
+            />
+          </Styled.IconButton>
+        </Styled.DropdownIndicator>
+      </Styled.AccordionTitle>
+      <Styled.AccordionContent
+        ref={accordionContentContainerRef}
+        expanded={expandedState}
+        height={height}
+      >
+        {children}
+      </Styled.AccordionContent>
+    </div>
+  );
+});
+
+export default Accordion;
---
+++ src/components/Accordion/styled.ts
@@ -0,0 +1,57 @@
+import Flex from "../Flex";
+import styled from "styled-components";
+
+export const AccordionTitle = styled(Flex)<{
+  expanded: boolean;
+  disabled?: boolean;
+}>`
+  background-color: ${({ expanded, disabled, theme }) =>
+    // eslint-disable-next-line no-nested-ternary
+    disabled
+      ? theme.palette.gray.light
+      : expanded
+      ? theme.palette.primary.highlight
+      : theme.palette.gray.highlight};
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.palette.text.disabled : "auto"};
+  cursor: ${({ disabled }) => (disabled ? "not-allowed" : "pointer")};
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
+  justify-content: space-between;
+  border-top: ${({ expanded, theme }) =>
+    !expanded ? `1px solid ${theme.palette.divider}` : "none"};
+  transition: background-color 0.3s, border-top 0.3s;
+`;
+
+export const AccordionTitleChildren = styled.div`
+  padding: ${({ theme }) => theme.spacing * 1.25}px;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+`;
+
+export const IconButton = styled.div<{ expanded: boolean }>`
+  padding: ${({ theme }) => `${theme.spacing * 1.25}px ${theme.spacing * 2}px`};
+  transition: transform 150ms;
+  transform-origin: center center;
+  transform: ${({ expanded }) => (expanded ? "rotate(180deg)" : "rotate(0)")};
+`;
+
+export const DropdownIndicator = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 0 0 auto;
+  width: ${({ theme }) => theme.spacing * 5}px;
+`;
+
+export const AccordionContent = styled.div<{
+  expanded: boolean;
+  height: number;
+}>`
+  height: ${({ height }) => height}px;
+  overflow: hidden;
+  transition: 0.3s all;
+`;
