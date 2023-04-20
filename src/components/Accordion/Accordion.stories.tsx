d35f9c5e73a92783939f475d1f955da4ae0b1039
takurinton <takurinton@takurinton.dev>
2023-04-20 11:40:48 +0900

    fix: `<Accordion />` story

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Story } from "@storybook/react/types-6-0";
+import { StoryObj } from "@storybook/react";
 import Accordion, { AccordionProps } from "./Accordion";
 
 export default {
@@ -13,8 +13,8 @@ export default {
   },
 };
 
-export const Example: Story<AccordionProps> = (args) => (
-  <>
+export const Example: StoryObj<AccordionProps> = {
+  render: (args) => (
     <Accordion
       title={args.title}
       disabled={args.disabled}
@@ -22,55 +22,13 @@ export const Example: Story<AccordionProps> = (args) => (
     >
       <div style={{ padding: "4px 8px" }}>{args.children}</div>
     </Accordion>
-  </>
-);
+  ),
+};
 
-export const Multiple: Story<AccordionProps> = () => (
-  <>
-    <Accordion title="title1">
-      <div style={{ padding: "4px 8px" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
-        magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
-        augue, efficitur eget tincidunt non, accumsan et est. Aliquam lobortis
-        elit lorem, id ullamcorper libero elementum non. Sed rutrum porta enim,
-        et aliquet velit hendrerit ac. Morbi tincidunt eleifend elit.
-      </div>
-    </Accordion>
-    <Accordion title="title2">
-      <div style={{ padding: "4px 8px" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
-        magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
-        augue, efficitur eget tincidunt non, accumsan et est. Aliquam lobortis
-        elit lorem, id ullamcorper libero elementum non. Sed rutrum porta enim,
-        et aliquet velit hendrerit ac. Morbi tincidunt eleifend elit.
-      </div>
-    </Accordion>
-    <Accordion disabled title="disabled">
-      <div style={{ padding: "4px 8px" }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
-        magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
-        augue, efficitur eget tincidunt non, accumsan et est. Aliquam lobortis
-        elit lorem, id ullamcorper libero elementum non. Sed rutrum porta enim,
-        et aliquet velit hendrerit ac. Morbi tincidunt eleifend elit.
-      </div>
-    </Accordion>
-  </>
-);
-
-export const Controlled: Story<AccordionProps> = () => {
-  const [expanded, setExpanded] = React.useState<string | false>(false);
-  const handleChange =
-    (panel: string) => (event: React.SyntheticEvent, newExpanded: boolean) => {
-      setExpanded(newExpanded ? panel : false);
-    };
-
-  return (
+export const Multiple: StoryObj<AccordionProps> = {
+  render: () => (
     <>
-      <Accordion
-        title="title1"
-        expanded={expanded === "title1"}
-        onChange={handleChange("title1")}
-      >
+      <Accordion title="title1">
         <div style={{ padding: "4px 8px" }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
           magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
@@ -79,11 +37,7 @@ export const Controlled: Story<AccordionProps> = () => {
           enim, et aliquet velit hendrerit ac. Morbi tincidunt eleifend elit.
         </div>
       </Accordion>
-      <Accordion
-        title="title2"
-        expanded={expanded === "title2"}
-        onChange={handleChange("title2")}
-      >
+      <Accordion title="title2">
         <div style={{ padding: "4px 8px" }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
           magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
@@ -92,11 +46,7 @@ export const Controlled: Story<AccordionProps> = () => {
           enim, et aliquet velit hendrerit ac. Morbi tincidunt eleifend elit.
         </div>
       </Accordion>
-      <Accordion
-        title="title3"
-        expanded={expanded === "title3"}
-        onChange={handleChange("title3")}
-      >
+      <Accordion disabled title="disabled">
         <div style={{ padding: "4px 8px" }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
           magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
@@ -106,5 +56,63 @@ export const Controlled: Story<AccordionProps> = () => {
         </div>
       </Accordion>
     </>
-  );
+  ),
+};
+
+export const Controlled: StoryObj<AccordionProps> = {
+  render: () => {
+    const [expanded, setExpanded] = React.useState<string | false>(false);
+    const handleChange =
+      (panel: string) =>
+      (event: React.SyntheticEvent, newExpanded: boolean) => {
+        setExpanded(newExpanded ? panel : false);
+      };
+
+    return (
+      <>
+        <Accordion
+          title="title1"
+          expanded={expanded === "title1"}
+          onChange={handleChange("title1")}
+        >
+          <div style={{ padding: "4px 8px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
+            magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
+            augue, efficitur eget tincidunt non, accumsan et est. Aliquam
+            lobortis elit lorem, id ullamcorper libero elementum non. Sed rutrum
+            porta enim, et aliquet velit hendrerit ac. Morbi tincidunt eleifend
+            elit.
+          </div>
+        </Accordion>
+        <Accordion
+          title="title2"
+          expanded={expanded === "title2"}
+          onChange={handleChange("title2")}
+        >
+          <div style={{ padding: "4px 8px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
+            magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
+            augue, efficitur eget tincidunt non, accumsan et est. Aliquam
+            lobortis elit lorem, id ullamcorper libero elementum non. Sed rutrum
+            porta enim, et aliquet velit hendrerit ac. Morbi tincidunt eleifend
+            elit.
+          </div>
+        </Accordion>
+        <Accordion
+          title="title3"
+          expanded={expanded === "title3"}
+          onChange={handleChange("title3")}
+        >
+          <div style={{ padding: "4px 8px" }}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc velit
+            magna, dictum vel elementum ac, fermentum quis lorem. Aliquam sapien
+            augue, efficitur eget tincidunt non, accumsan et est. Aliquam
+            lobortis elit lorem, id ullamcorper libero elementum non. Sed rutrum
+            porta enim, et aliquet velit hendrerit ac. Morbi tincidunt eleifend
+            elit.
+          </div>
+        </Accordion>
+      </>
+    );
+  },
 };
