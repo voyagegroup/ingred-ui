23a5ff412d957046e279014541ad8225c47d1078
takurinton <takurinton@takurinton.dev>
2023-04-26 16:27:14 +0900

    wip: `useLocaleProps.ts`

--- src/hooks/useLocaleProps.ts
+++ src/hooks/useLocaleProps.ts
@@ -1,5 +1,6 @@
 import React from "react";
 import { LocaleContext } from "../components/LocaleProvider/LocaleProvider";
+import { LocalizationComponentName } from "../constants/locale";
 
 /**
  * Overwrite the props by locale if there is no params props.
@@ -8,18 +9,24 @@ import { LocaleContext } from "../components/LocaleProvider/LocaleProvider";
  */
 export function useLocaleProps<T>(params: {
   props: React.PropsWithChildren<T>;
-  name: string;
+  name: LocalizationComponentName;
 }) {
   const { name, props } = params;
   const localeContext = React.useContext(LocaleContext);
   const locale = localeContext.locale;
-
   // Resolve and merge locale props
   const output = { ...props };
-  const defaultProps = locale?.components?.[name].defaultProps;
+  if (!locale || !locale.components) {
+    return output;
+  }
+  const defaultProps = locale.components?.[name]?.defaultProps;
 
   for (const propName in defaultProps) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     if (output[propName] === undefined) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
       output[propName] = defaultProps[propName];
     }
   }
