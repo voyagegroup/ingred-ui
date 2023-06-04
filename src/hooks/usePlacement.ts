cbc6149e8fe0314c0aea0a2d3357c69656d34273
penicillin0 <ayahitonakamura@gmail.com>
2023-06-04 23:20:14 +0900

    add usePlacement

---
+++ src/hooks/usePlacement.ts
@@ -0,0 +1,19 @@
+import { Placement } from "@floating-ui/react";
+
+export const autoPlacements = ["auto", "auto-start", "auto-end"] as const;
+export type AutoPlacement = typeof autoPlacements[number];
+
+export const usePlacement = (placements: (Placement | AutoPlacement)[]) => {
+  const placementsWithoutAuto = placements.filter(
+    (placement): placement is Placement =>
+      !autoPlacements.includes(placement as AutoPlacement),
+  );
+  const isAuto = placements.some((placement) =>
+    autoPlacements.includes(placement as AutoPlacement),
+  );
+
+  return {
+    placements: placementsWithoutAuto,
+    isAuto,
+  };
+};
