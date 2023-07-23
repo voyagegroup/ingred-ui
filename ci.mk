2b7727b1a8aed5c78b0465eff65d85fc03959235
Nakamura Ayahito <56141035+penicillin0@users.noreply.github.com>
2023-07-23 09:23:38 +0900

    changesetsの導入 (#1298)

--- ci.mk
+++ ci.mk
@@ -24,6 +24,9 @@ lint:
 build:
 	yarn build
 
+check_changesets:
+	yarn changeset status --since origin/master
+
 release_version:
 	npm config set git-tag-version false
 	npm version ${RELEASE_VERSION}
