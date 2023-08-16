6e92fd7f14ba1756c2850a9af4e8c20c6bdaa9fa
takurinton <takurinton@takurinton.dev>
2023-08-16 12:48:14 +0900

    add: date range picker

---
+++ src/components/NewDateRangePicker/NewDateRangePicker.stories.tsx
@@ -0,0 +1,80 @@
+import { StoryObj } from "@storybook/react";
+import NewDateRangePicker, {
+  NewDateRangePickerProps,
+} from "./NewDateRangePicker";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+
+export default {
+  title: "Components/Inputs/NewDateRangePicker",
+  component: NewDateRangePicker,
+  args: {
+    format: "YYYY-MM-DD",
+  },
+};
+
+export const Example: StoryObj<NewDateRangePickerProps> = {
+  render: (args) => {
+    const [date, setDate] = useState({
+      startDate: dayjs(),
+      endDate: dayjs().add(1, "week"),
+    });
+
+    return (
+      <NewDateRangePicker
+        {...args}
+        startDate={date.startDate}
+        endDate={date.endDate}
+        onDatesChange={setDate}
+      />
+    );
+  },
+};
+
+export const WithActions: StoryObj<NewDateRangePickerProps> = {
+  render: (args) => {
+    const [date, setDate] = useState({
+      startDate: dayjs(),
+      endDate: dayjs().add(1, "week"),
+    });
+    const actions = [
+      {
+        text: "今日",
+        onClick: () => {
+          setDate({
+            startDate: dayjs(),
+            endDate: dayjs(),
+          });
+        },
+      },
+      {
+        text: "明日",
+        onClick: () => {
+          setDate({
+            startDate: dayjs(),
+            endDate: dayjs().add(1, "day"),
+          });
+        },
+      },
+      {
+        text: "来週",
+        onClick: () => {
+          setDate({
+            startDate: dayjs(),
+            endDate: dayjs().add(1, "week"),
+          });
+        },
+      },
+    ];
+
+    return (
+      <NewDateRangePicker
+        {...args}
+        actions={actions}
+        startDate={date.startDate}
+        endDate={date.endDate}
+        onDatesChange={setDate}
+      />
+    );
+  },
+};
---
+++ src/components/NewDateRangePicker/NewDateRangePicker.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import { Flex, DateRangeField, CalendarRange, Card } from "..";
+import { forwardRef, useRef, useState } from "react";
+import {
+  flip,
+  offset,
+  useDismiss,
+  useFloating,
+  useInteractions,
+  useRole,
+} from "@floating-ui/react";
+import { Dayjs } from "dayjs";
+import { Action, Actions } from "../Calendar/internal/Actions";
+
+export type NewDateRangePickerProps = {
+  startDate: Dayjs;
+  endDate: Dayjs;
+  actions?: Action[];
+  onDatesChange: (date: { startDate: Dayjs; endDate: Dayjs }) => void;
+};
+
+export const DateRangePicker = forwardRef<
+  HTMLDivElement,
+  NewDateRangePickerProps
+>(function DateRangePicker({ startDate, endDate, actions, onDatesChange }) {
+  const [open, setOpen] = useState(false);
+  const { context, refs, strategy, x, y } = useFloating({
+    placement: "right-start",
+    open,
+    onOpenChange: setOpen,
+    middleware: [offset(10), flip()],
+  });
+  const ref = useRef<HTMLDivElement>(null);
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([
+    dismiss,
+    role,
+  ]);
+
+  const handleClickCalendarIcon = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <Flex ref={ref}>
+      <div
+        ref={refs.setReference}
+        {...getReferenceProps()}
+        style={{ width: "fit-content" }}
+      >
+        <DateRangeField
+          date={{
+            startDate,
+            endDate,
+          }}
+          onDatesChange={onDatesChange}
+          onClickCalendarIcon={handleClickCalendarIcon}
+        />
+      </div>
+      {open && (
+        <Card
+          ref={refs.setFloating}
+          display="flex"
+          style={{
+            position: strategy,
+            top: y ?? 0,
+            left: x ?? 0,
+            zIndex: 1,
+          }}
+          {...getFloatingProps()}
+        >
+          <Actions actions={actions} />
+          <CalendarRange
+            startDate={startDate}
+            endDate={endDate}
+            onDatesChange={onDatesChange}
+          />
+        </Card>
+      )}
+    </Flex>
+  );
+});
+
+export default React.memo(DateRangePicker);
