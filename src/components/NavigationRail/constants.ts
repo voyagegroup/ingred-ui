a34ca231bc089c24bb5f876ba54f6a3c18dba631
takurinton <takurinton@takurinton.dev>
2023-08-28 13:33:09 +0900

    enum to const

--- src/components/NavigationRail/constants.ts
+++ src/components/NavigationRail/constants.ts
@@ -1,13 +1,12 @@
-export enum NavigationRailWidth {
-  NARROW = "74px",
-  WIDE = "256px",
-}
+export const NavigationRailWidth = {
+  NARROW: "74px",
+  WIDE: "256px",
+} as const;
 
-export enum NavigationRailContentHeight {
-  HEADER = "72px",
-  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values
-  FOOTER = "72px",
-}
+export const NavigationRailContentHeight = {
+  HEADER: "72px",
+  FOOTER: "72px",
+} as const;
 
 // Unit: second
 export const NavigationRailTransitionDuration = 0.3;
