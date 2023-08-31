0de5aa7d02e820f95a61ad0d984c964fff660d20
takurinton <takurinton@takurinton.dev>
2023-08-31 15:23:01 +0900

    remove weekList constants

--- src/components/Calendar/constants.ts
+++ src/components/Calendar/constants.ts
@@ -1,9 +1,2 @@
-import dayjs from "dayjs";
-
 // for `<ScrollArea />`
 export const HEIGHT = "400px";
-
-const dayIndex = dayjs().day();
-const startDay = dayjs().subtract(dayIndex, "day");
-
-export const weekList = [...Array(7)].map((_, i) => startDay.add(i, "day"));
