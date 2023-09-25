79ed9f7c4d1e35d754eec27729182bed034b1a76
takurinton <takurinton@takurinton.dev>
2023-09-25 13:06:39 +0900

    fix: @testing-library/jest-dom/extend-expect to @testing-library/jest-dom

--- src/components/ContextMenu/__tests__/ContextMenu.test.tsx
+++ src/components/ContextMenu/__tests__/ContextMenu.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import { cleanup, fireEvent, act } from "@testing-library/react";
 import { renderWithThemeProvider } from "../../../utils/renderWithThemeProvider";
 import ContextMenu from "../ContextMenu";
--- src/components/DateRangePicker/__tests__/DateRangePicker.test.tsx
+++ src/components/DateRangePicker/__tests__/DateRangePicker.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 import DateRangePicker from "..";
 import { renderWithThemeProvider } from "../../../utils/renderWithThemeProvider";
