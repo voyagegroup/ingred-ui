841f313a5223253d2c6079fbfc3652bbee249ca3
takurinton <takurinton@takurinton.dev>
2023-09-28 13:30:15 +0900

    fix style
    fix style

--- src/components/WeekTimeSelector/styled.ts
+++ src/components/WeekTimeSelector/styled.ts
@@ -39,11 +39,18 @@ export const WeekTimeItem = styled.button<{ active: boolean; hover: boolean }>`
       ? theme.palette.primary.highlight
       : active
       ? theme.palette.primary.main
-      : theme.palette.gray.highlight};
+      : theme.palette.white};
+  border: 1px solid
+    ${({ theme, active, hover }) =>
+      // eslint-disable-next-line no-nested-ternary
+      hover
+        ? theme.palette.primary.light
+        : active
+        ? theme.palette.primary.dark
+        : theme.palette.gray.main};
   border-radius: ${({ theme }) => theme.spacing / 2}px;
   cursor: pointer;
   height: ${({ theme }) => theme.spacing * 3}px;
   width: ${({ theme }) => theme.spacing * 3}px;
-  border: none;
   outline: none;
 `;
