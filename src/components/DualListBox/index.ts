251df4c9a208335f43c8c33c0d7c526d2fd4818b
takurinton <takurinton@takurinton.dev>
2023-10-12 10:12:02 +0900

    Update src/components/DualListBox/index.ts
    Co-authored-by: Yutaro Hayashi <yut.hayashi@cartahd.com>

--- src/components/DualListBox/index.ts
+++ src/components/DualListBox/index.ts
@@ -1,2 +1,2 @@
 export { default } from "./DualListBox";
-export type { DualListBoxProps, DualListBoxItem } from "./DualListBox";
+export type { DualListBoxProps, DualListBoxItem, DualListBoxSelectedItem } from "./DualListBox";
