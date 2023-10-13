9766186c9e22f87553d93905d4ce3321c7338b76
yutan <71750401+Yutan0423@users.noreply.github.com>
2023-10-13 10:43:57 +0900

    rm style

--- src/components/WeekTime/WeekTime/styled.ts
+++ src/components/WeekTime/WeekTime/styled.ts
@@ -2,35 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const WeekTimeContainer = styled.div`
-  display: grid;
-  grid-template-columns: 38px repeat(24, 1fr);
-  grid-gap: ${({ theme }) => theme.spacing / 2}px;
-  align-items: center;
-  justify-items: center;
-  width: fit-content;
-`;
-
-export const ContainerBase = styled.div`
-  height: 24px;
-  text-align: center;
-  background-color: ${({ theme }) => theme.palette.gray.light};
-  border-radius: ${({ theme }) => theme.spacing / 2}px;
-  border: 1px solid ${({ theme }) => theme.palette.gray.main};
-`;
-
-export const WeekContainer = styled(ContainerBase)`
-  width: 38px;
-`;
-
-export const TimeContainer = styled(ContainerBase)`
-  width: 24px;
-`;
-
-export const EmptyContainer = styled(ContainerBase)`
-  border: none;
-`;
-
 export const WeekTimeItem = styled.span<{ active: boolean }>`
   background-color: ${({ active, theme }) =>
     // eslint-disable-next-line no-nested-ternary
