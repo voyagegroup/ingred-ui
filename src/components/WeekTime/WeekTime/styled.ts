683cacd8b62014a7337ea7059f6bfbb37c48bcd8
takurinton <takurinton@takurinton.dev>
2023-10-14 12:15:59 +0900

    remove unnecessary eslint-disable

--- src/components/WeekTime/WeekTime/styled.ts
+++ src/components/WeekTime/WeekTime/styled.ts
@@ -4,11 +4,9 @@ export const Container = styled.div``;
 
 export const WeekTimeItem = styled.span<{ active: boolean }>`
   background-color: ${({ active, theme }) =>
-    // eslint-disable-next-line no-nested-ternary
     active ? theme.palette.primary.main : theme.palette.white};
   border: 1px solid
     ${({ theme, active }) =>
-      // eslint-disable-next-line no-nested-ternary
       active ? theme.palette.primary.dark : theme.palette.gray.main};
   border-radius: ${({ theme }) => theme.spacing / 2}px;
   height: ${({ theme }) => theme.spacing * 3}px;
