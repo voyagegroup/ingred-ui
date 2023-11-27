3e49cb877bf6398f3dd2b35ab435e7f56c8c69fd
noronaoki <ca00495@cartahd.com>
2023-11-27 10:25:34 +0900

    スタイルの変更

--- src/components/NotificationBadge/styled.ts
+++ src/components/NotificationBadge/styled.ts
@@ -33,28 +33,27 @@ export const Badge = styled.span<BadgeProps>`
   transform: ${({ invisible }) => (invisible ? "scale(0)" : "scale(1)")}
     translate(
       ${({ position }) =>
-        ["top-right", "bottom-right"].includes(position) ? "50%" : "-50%"},
+    ["top-right", "bottom-right"].includes(position) ? "50%" : "-50%"},
       ${({ position }) =>
-        ["bottom-right", "bottom-left"].includes(position) ? "50%" : "-50%"}
+    ["bottom-right", "bottom-left"].includes(position) ? "50%" : "-50%"}
     );
   transform-origin: ${({ position }) =>
-    `${["top-right", "bottom-right"].includes(position) ? "100%" : "0%"} ${
-      ["bottom-right", "bottom-left"].includes(position) ? "100%" : "0%"
+    `${["top-right", "bottom-right"].includes(position) ? "100%" : "0%"} ${["bottom-right", "bottom-left"].includes(position) ? "100%" : "0%"
     }`};
   display: flex;
   align-items: center;
   align-content: center;
   justify-content: center;
-  height: ${({ variant, size }) =>
-    variant === "normal" ? "20px" : dotSizeMapping[size]};
+  min-height: ${({ variant, size }) =>
+    variant === "normal" ? "24px" : dotSizeMapping[size]};
   min-width: ${({ variant, size }) =>
-    variant === "normal" ? "20px" : dotSizeMapping[size]};
+    variant === "normal" ? "24px" : dotSizeMapping[size]};
   line-height: 1;
-  padding: 0 ${({ variant }) => (variant === "normal" ? "6px" : 0)};
+  padding: 0 ${({ variant }) => (variant === "normal" ? "4px" : 0)};
   border-radius: 10rem;
   color: ${({ theme }) => theme.palette.text.white};
   background-color: ${({ theme }) => theme.palette.danger.main};
-  font-size: 10.5px;
+  font-size: 13px;
   font-weight: bold;
   transition: transform 0.3s;
 `;
