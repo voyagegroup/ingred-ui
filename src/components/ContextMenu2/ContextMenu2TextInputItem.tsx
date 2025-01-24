8c397c7cf8dd6e515cd0b8393e8814885da951e0
Yomotsu <admin@yomotsu.net>
2025-01-24 13:18:40 +0900

    update snapshot

--- src/components/ContextMenu2/ContextMenu2TextInputItem.tsx
+++ src/components/ContextMenu2/ContextMenu2TextInputItem.tsx
@@ -38,7 +38,7 @@ InternalContextMenu2TextInputItem.displayName = "ContextMenu2TextInputItem";
 export const ContextMenu2TextInputItem = styled(
   InternalContextMenu2TextInputItem,
 )`
-  padding: 6px 0;
+  padding: 6px 8px;
 
   input {
     display: block;
