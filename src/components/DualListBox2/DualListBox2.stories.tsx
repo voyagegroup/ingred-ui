7928b6d9e618b1bbd496cc7f2316260392706cb6
Yomotsu <admin@yomotsu.net>
2025-02-04 09:39:20 +0900

    stroybook 拡充

--- src/components/DualListBox2/DualListBox2.stories.tsx
+++ src/components/DualListBox2/DualListBox2.stories.tsx
@@ -12,6 +12,7 @@ import {
   ContextMenu2ButtonItem,
   ContextMenu2SwitchItem,
 } from "../ContextMenu2";
+import Checkbox from "../Checkbox";
 
 const meta = {
   title: "Components/Data Display/DualListBox2",
@@ -46,6 +47,19 @@ const toGroupedItems = (items: Item[]) => {
   return groupedItems;
 };
 
+/**
+ * #### ベーシックなタイプ
+ *
+ * 件数の上限が不明で都度サーバへ問い合わせる必要がある場合、上限が決まっている場合いずれにも使えます。
+ *
+ * ---
+ *
+ * `included` と `excluded` で選択中の値を指定します。
+ * 選択状態変更時は、`onIncludedChange` と `onExcludedChange` で、新しい `included` と `excluded` を受け取れるので、これを使って `included` と `excluded` を更新してください。
+ *
+ * children には、左パネル用の選択できる項目を渡します。右パネルの内容は状態に応じて自動で表示管理されます。
+ *
+ */
 export const Default: StoryObj<typeof meta> = {
   args: {
     included: [
@@ -150,6 +164,12 @@ export const Default: StoryObj<typeof meta> = {
   },
 };
 
+/**
+ * #### 選択候補がアコーディオンで折りたたまれているタイプ
+ *
+ * 件数の上限が決まっていて、多い場合や区切りたい場合に利用します。
+ * セクションごとに一括選択もできます。
+ */
 export const Accordion: StoryObj<typeof DualListBox2> = {
   render: () => {
     const [items, setItems] = useState<Item[]>([
@@ -232,6 +252,10 @@ export const Accordion: StoryObj<typeof DualListBox2> = {
   },
 };
 
+/**
+ * 「追加」「除外」のいずれかしかできない場合の例です。
+ * 状況に応じて、`disableExclude`（除外無効）、`disableInclude`（追加無効） に `true` を指定して制御してください。
+ */
 export const Either: StoryObj<typeof DualListBox2> = {
   render: () => {
     const [items, setItems] = useState<Item[]>([
@@ -261,64 +285,96 @@ export const Either: StoryObj<typeof DualListBox2> = {
     const [included, setIncluded] = useState<Item[]>([items[0]]);
     const [excluded, setExcluded] = useState<Item[]>([items[3]]);
     const [isLoading, setIsLoading] = useState(false);
+    const [disableInclude, setDisableInclude] = useState(false);
+    const [disableExclude, setDisableExclude] = useState(true);
     return (
-      <DualListBox2
-        disableExclude
-        loading={isLoading}
-        included={included}
-        excluded={excluded}
-        menuButtons={
-          <>
-            <ContextMenu2ButtonItem
-              onClick={() => {
-                alert("clicked");
-              }}
-            >
-              好きなボタンを
-            </ContextMenu2ButtonItem>
-            <ContextMenu2SwitchItem disabled onChange={() => {}}>
-              入れて使う
-            </ContextMenu2SwitchItem>
-          </>
-        }
-        onIncludedChange={(ids: string[]) =>
-          setIncluded(items.filter((item) => ids.includes(item.id)))
-        }
-        onExcludedChange={(ids: string[]) =>
-          setExcluded(items.filter((item) => ids.includes(item.id)))
-        }
-        onLoadMore={() => {
-          setIsLoading(true);
-          setTimeout(() => {
-            setItems((prev) => [
-              ...prev,
-              ...generateItems(prev.length + 1, 10, "アコーディオン2"),
-            ]);
-            setIsLoading(false);
-          }, 1000);
-        }}
-      >
-        {toGroupedItems(items).map(
-          (group) =>
-            group.groupName && (
-              <DualListBox2Accordion
-                key={group.groupName}
-                disableExclude
-                label={group.groupName}
+      <>
+        <Checkbox
+          checked={disableInclude}
+          onChange={() => setDisableInclude(!disableInclude)}
+        >
+          disableInclude
+        </Checkbox>
+        <Checkbox
+          checked={disableExclude}
+          onChange={() => setDisableExclude(!disableExclude)}
+        >
+          disableExclude
+        </Checkbox>
+
+        <DualListBox2
+          disableExclude={disableExclude}
+          disableInclude={disableInclude}
+          loading={isLoading}
+          included={included}
+          excluded={excluded}
+          menuButtons={
+            <>
+              <ContextMenu2ButtonItem
+                onClick={() => {
+                  alert("clicked");
+                }}
               >
-                {group.items.map((item) => (
-                  <DualListBox2Item key={item.id} disableExclude id={item.id}>
-                    {item.label}
-                  </DualListBox2Item>
-                ))}
-              </DualListBox2Accordion>
-            ),
-        )}
-      </DualListBox2>
+                好きなボタンを
+              </ContextMenu2ButtonItem>
+              <ContextMenu2SwitchItem disabled onChange={() => {}}>
+                入れて使う
+              </ContextMenu2SwitchItem>
+            </>
+          }
+          onIncludedChange={(ids: string[]) =>
+            setIncluded(items.filter((item) => ids.includes(item.id)))
+          }
+          onExcludedChange={(ids: string[]) =>
+            setExcluded(items.filter((item) => ids.includes(item.id)))
+          }
+          onLoadMore={() => {
+            setIsLoading(true);
+            setTimeout(() => {
+              setItems((prev) => [
+                ...prev,
+                ...generateItems(prev.length + 1, 10, "アコーディオン2"),
+              ]);
+              setIsLoading(false);
+            }, 1000);
+          }}
+        >
+          {toGroupedItems(items).map(
+            (group) =>
+              group.groupName && (
+                <DualListBox2Accordion
+                  key={group.groupName}
+                  disableExclude={disableExclude}
+                  disableInclude={disableInclude}
+                  label={group.groupName}
+                >
+                  {group.items.map((item) => (
+                    <DualListBox2Item
+                      key={item.id}
+                      disableExclude={disableExclude}
+                      disableInclude={disableInclude}
+                      id={item.id}
+                    >
+                      {item.label}
+                    </DualListBox2Item>
+                  ))}
+                </DualListBox2Accordion>
+              ),
+          )}
+        </DualListBox2>
+      </>
     );
   },
 };
 
+/**
+ * #### 選択候補がセクションで分かれているタイプ
+ *
+ * 件数の上限が不明で都度サーバへ問い合わせる必要がある場合に利用します。
+ * 検索は各セクションに移動後に使えます。
+ *
+ * 任意のセクション選択中は、その他のセクションは非表示となります。
+ */
 export const Section: StoryObj<typeof DualListBox2> = {
   render: () => {
     const [items, setItems] = useState<Item[]>([
