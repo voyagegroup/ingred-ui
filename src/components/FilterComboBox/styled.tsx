1475ca9c9607b2176f9e500862980510943a07d4
noronaoki <ca00495@cartahd.com>
2025-03-13 14:17:38 +0900

    useThemeの削除

--- src/components/FilterComboBox/styled.tsx
+++ src/components/FilterComboBox/styled.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { colors } from "../../styles";
-import { useTheme } from "../../themes";
 
 export const SelectContainer = styled.div`
   position: relative;
