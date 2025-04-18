5aa721f3d5487430944de9c06a6d6e1ca9640ce1
noronaoki <ca00495@cartahd.com>
2025-04-18 20:22:45 +0900

    不要なコメントアウトを削除

--- src/components/ContextMenu2/ContextMenu2TextInputItem.tsx
+++ src/components/ContextMenu2/ContextMenu2TextInputItem.tsx
@@ -118,7 +118,6 @@ InternalContextMenu2TextInputItem.displayName = "ContextMenu2TextInputItem";
 export const ContextMenu2TextInputItem = styled(
   InternalContextMenu2TextInputItem,
 )`
-  // padding: 6px 8px;
 
   input {
     display: block;
