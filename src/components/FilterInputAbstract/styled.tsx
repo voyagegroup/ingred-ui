ec4f58be952cd135564c1be2a05ad543f335a5fe
noronaoki <ca00495@cartahd.com>
2025-05-23 13:23:39 +0900

    Tagコンポーネント化によって不要になったstyleの指定が残っていたので削除。

--- src/components/FilterInputAbstract/styled.tsx
+++ src/components/FilterInputAbstract/styled.tsx
@@ -38,10 +38,6 @@ export const getTriggerIconSizeStyle = (size: FilterSize) => css`
   padding: ${FILTER_SIZES[size].padding};
 `;
 
-export type FilterTagProps = {
-  $size: FilterSize;
-};
-
 export const FilterInputAbstract = styled.div<{
   $isOpen?: boolean;
   $error?: boolean;
@@ -171,93 +167,6 @@ export const DropDownTrigger = styled.button`
 
   &:where(${FilterInputAbstract.toString()}[data-small="true"] *) {
     display: none;
-  }
-`;
-
-export const FilterTag = styled.span<FilterTagProps>`
-  isolation: isolate;
-  display: flex;
-  align-items: center;
-  gap: 4px;
-  width: fit-content;
-  padding: ${({ $size }) => {
-    switch ($size) {
-      case "small":
-        return "2px 4px 2px 6px";
-      case "medium":
-        return "2px 5px 2px 7px";
-      case "large":
-        return "3px 6px 3px 8px";
-    }
-  }};
-  border: 1px solid ${colors.basic[400]};
-  border-radius: 2px;
-  font-weight: 400;
-  font-size: ${({ $size }) => {
-    switch ($size) {
-      case "small":
-        return "11px";
-      case "medium":
-        return "12px";
-      case "large":
-        return "13px";
-    }
-  }};
-  line-height: 14px;
-  word-break: break-all;
-  color: ${colors.basic[900]};
-  background-color: ${colors.basic[0]};
-`;
-
-export const FilterTagButton = styled.button<FilterTagProps>`
-  flex-shrink: 0;
-  height: ${({ $size }) => {
-    switch ($size) {
-      case "small":
-        return "14px";
-      case "medium":
-        return "16px";
-      case "large":
-        return "18px";
-    }
-  }};
-  aspect-ratio: 1;
-  padding: 0;
-  border: 0;
-  color: ${colors.basic[900]};
-  background-color: transparent;
-  cursor: pointer;
-
-  /* アイコンのサイズ調整 */
-  svg {
-    width: ${({ $size }) => {
-      switch ($size) {
-        case "small":
-          return "14px";
-        case "medium":
-          return "16px";
-        case "large":
-          return "18px";
-      }
-    }};
-    height: ${({ $size }) => {
-      switch ($size) {
-        case "small":
-          return "14px";
-        case "medium":
-          return "16px";
-        case "large":
-          return "18px";
-      }
-    }};
-  }
-
-  span {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
   }
 `;
 
